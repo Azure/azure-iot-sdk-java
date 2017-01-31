970d4cfd57bda97ec6665f1032e2d9b3b5173cf1
Anil Hashia <anhashia@microsoft.com>
2017-01-30 16:52:08 -0800

    Fix encoding issue in MessagePropertyTest.java

--- device/iot-device-client/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/device/MessagePropertyTest.java
+++ device/iot-device-client/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/device/MessagePropertyTest.java
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+//Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package tests.unit.com.microsoft.azure.sdk.iot.device;
