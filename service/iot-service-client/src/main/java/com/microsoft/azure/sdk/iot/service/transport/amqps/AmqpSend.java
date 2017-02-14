df22b7d6054f08bdeefebaebc08dec07510f445a
Enes Uysal <enuysa@microsoft.com>
2017-02-14 13:23:57 +0000

    Update AmqpSend.java
    this.reactor.run(); is removed from the line #114

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/transport/amqps/AmqpSend.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/transport/amqps/AmqpSend.java
@@ -110,8 +110,7 @@ public class AmqpSend extends BaseHandler
             amqpSendHandler.createProtonMessage(deviceId, message);
             // Codes_SRS_SERVICE_SDK_JAVA_AMQPSEND_12_007: [The function shall initialize the Proton reactor object]
             this.reactor = Proton.reactor(this);
-            // Codes_SRS_SERVICE_SDK_JAVA_AMQPSEND_12_008: [The function shall start the Proton reactor object]
-            this.reactor.run();
+            // Codes_SRS_SERVICE_SDK_JAVA_AMQPSEND_12_008: [The function shall start the Proton reactor object
             try
             {
               this.reactor.run();
