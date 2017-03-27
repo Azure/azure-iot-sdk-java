33eb7e8e39bc5afe53aeaeba0dbdcbd3d35af096
mamokarz <mamokarz@microsoft.com>
2017-03-27 14:41:35 -0700

    Create an independent Integration Test module for service and device clients, removing tests and its dependencies from the published package

--- jenkins/windows_java.cmd
+++ jenkins/windows_java.cmd
@@ -7,7 +7,7 @@ set build-root=%~dp0..
 rem // resolve to fully qualified path
 for %%i in ("%build-root%") do set build-root=%%~fi
 
-REM -- Websocket Transport Layer --
+REM -- Java Dependencies --
 cd %build-root%\deps
 call mvn install
 if errorlevel 1 goto :eof
@@ -15,12 +15,24 @@ cd %build-root%
 
 REM -- Java Service Client --
 cd %build-root%\service
+call mvn install
+if errorlevel 1 goto :eof
+cd %build-root%
+
+REM -- Java Service Client - integration tests --
+cd %build-root%\iot-service-tests
 call mvn install -DskipITs=false
 if errorlevel 1 goto :eof
 cd %build-root%
 
 REM -- Java Device Client --
 cd %build-root%\device
+call mvn install
+if errorlevel 1 goto :eof
+cd %build-root%
+
+REM -- Java Device Client - integration tests --
+cd %build-root%\iot-device-tests
 call mvn install -DskipITs=false
 if errorlevel 1 goto :eof
 cd %build-root%
