36b0149c87fee429ede1b0b87d32041ca398fe60
mamokarz <mamokarz@microsoft.com>
2017-06-23 13:04:21 -0700

    Small fix for file upload and query parsers

--- deps/serializer/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/QueryResponseParser.java
+++ deps/serializer/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/QueryResponseParser.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
+
 import java.util.LinkedList;
 import java.util.List;
 
