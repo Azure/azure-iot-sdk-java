0508a43f8cea8e2ed2b64c6d2838c268bd75ea00
prmathur-microsoft <prmathur@microsoft.com>
2017-06-29 13:51:58 -0700

    Modify Query serilizer to support latest

--- deps/serializer/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/QueryResponseParser.java
+++ deps/serializer/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/QueryResponseParser.java
@@ -9,7 +9,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
-
 import java.util.LinkedList;
 import java.util.List;
 
