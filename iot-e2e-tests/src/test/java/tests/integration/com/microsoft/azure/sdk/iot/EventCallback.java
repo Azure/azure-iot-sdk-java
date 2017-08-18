480a1a38fffe845c92350ed6f0e37ad49415680b
prmathur-microsoft <prmathur@microsoft.com>
2017-08-17 18:09:17 -0700

    Move E2E to common package

---
+++ iot-e2e-tests/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/EventCallback.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package tests.integration.com.microsoft.azure.sdk.iot;
+
+import com.microsoft.azure.sdk.iot.device.IotHubEventCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubStatusCode;
+
+public class EventCallback implements IotHubEventCallback
+{
+    public void execute(IotHubStatusCode status, Object context)
+    {
+        Success success = (Success) context;
+        if (status.equals(IotHubStatusCode.OK_EMPTY))
+        {
+            success.setResult(true);
+        }
+        else
+        {
+            success.setResult(false);
+        }
+    }
+}
+
