ea5fa944ab9f2258c1d7ae55a8fef6fe3b6c0434
Ranjith Akuthota <ranjith.a@zen3.com>
2017-09-29 23:48:39 +0530

    kill avd before starting new one

--- iot-e2e-tests/android/AndroidDeviceSelect.py
+++ iot-e2e-tests/android/AndroidDeviceSelect.py
@@ -3,39 +3,52 @@ import os
 import time
 
 
-def getAndroidDeviceUDID():
+def startAvd():
+    isEmulatorImagePresent = False
+    emulatorList = os.popen("emulator -list-avds").read()
+    formatted_string = emulatorList.strip().split('\n')
+    for x in formatted_string:
+        if "test" in x:
+            isEmulatorImagePresent = True
+            break
+    if isEmulatorImagePresent:
+        os.popen("start cmd.exe /k emulator @test")
+        time.sleep(30)
+        writeToFile((getDeviceList()[0]).split()[0])
+    else:
+        os.popen("echo no | avdmanager create avd -n test -k \"system-images;android-25;google_apis;x86\"").read()
+        time.sleep(10)
+        startAvd()
+
+
+def writeToFile(deviceName):
+    os.popen("echo " + deviceName + ">device_udid.txt").read()
+
+def getDeviceList():
     res = os.popen("adb devices").read()
     formatted_string = res.strip().split('\n')
     deviceList = []
     for x in formatted_string:
         if "\tdevice" in x:
             deviceList.append(x)
-    device_count = deviceList.__len__()
-    if device_count == 0:
-        startAvd()
-    else:
-        writeToFile(deviceList[0].split()[0])
+    return deviceList
 
-
-def startAvd():
-    isEmulatorPresent = False
-    emulatorList = os.popen("emulator -list-avds").read()
-    formatted_string = emulatorList.strip().split('\n')
-    for x in formatted_string:
-        if "test1" in x:
-            isEmulatorPresent = True
+def killAvd():
+    hasRealDevice = False
+    deviceList = getDeviceList()
+    for device in deviceList:
+        if not device.startswith('emulator'):
+            hasRealDevice = True
+            writeToFile(device)
             break
-
-    if isEmulatorPresent:
-        os.popen("start cmd.exe /k emulator @test")
-        time.sleep(30)
-        getAndroidDeviceUDID()
-    else:
-        res = os.popen("echo no | avdmanager create avd -n test -k \"system-images;android-25;google_apis;x86\"").read()
-        startAvd()
+    if not hasRealDevice:
+        for device in deviceList:
+            os.popen("adb -s " + device.split()[0] + " emu kill")
+    startAvd()
 
 
-def writeToFile(deviceName):
-    os.popen("echo " + deviceName + ">device_udid.txt").read()
-	
-getAndroidDeviceUDID()
+killAvd()
+
+
+
+
