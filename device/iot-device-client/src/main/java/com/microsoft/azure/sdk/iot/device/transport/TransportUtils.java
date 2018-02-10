54a75c40407edb8adb1490f8a2a61979d1769861
Marcos Mokarzel <mamokarz@users.noreply.github.com>
2018-02-09 16:08:46 -0800

    Bump package versions for release 2018-2-9

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/TransportUtils.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/TransportUtils.java
@@ -6,7 +6,7 @@ package com.microsoft.azure.sdk.iot.device.transport;
 public class TransportUtils
 {
     public static final String JAVA_DEVICE_CLIENT_IDENTIFIER = "com.microsoft.azure.sdk.iot.iot-device-client/";
-    public static final String CLIENT_VERSION = "1.7.0";
+    public static final String CLIENT_VERSION = "1.8.0";
 
     private static final byte[] SLEEP_INTERVALS = {1, 2, 4, 8, 16, 32, 60};
     /** Generates a reconnection time with an exponential backoff
