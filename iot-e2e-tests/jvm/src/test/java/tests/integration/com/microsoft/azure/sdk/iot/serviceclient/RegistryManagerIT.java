20adcf85a973d4d18949865dba88d22b2dc85f7b
Jasmine Lo <jasminel@microsoft.com>
2018-06-26 14:27:46 -0700

    feat(e2e): add e2e for module and configuration

--- iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/serviceclient/RegistryManagerIT.java
+++ iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/serviceclient/RegistryManagerIT.java
@@ -5,10 +5,10 @@
 
 package tests.integration.com.microsoft.azure.sdk.iot.serviceclient;
 
-import com.microsoft.azure.sdk.iot.service.Device;
-import com.microsoft.azure.sdk.iot.service.DeviceStatus;
-import com.microsoft.azure.sdk.iot.service.RegistryManager;
+import com.microsoft.azure.sdk.iot.service.*;
 import com.microsoft.azure.sdk.iot.service.auth.AuthenticationType;
+import com.microsoft.azure.sdk.iot.service.auth.SymmetricKey;
+import com.microsoft.azure.sdk.iot.service.exceptions.IotHubBadFormatException;
 import com.microsoft.azure.sdk.iot.service.exceptions.IotHubException;
 import com.microsoft.azure.storage.StorageException;
 import org.junit.AfterClass;
@@ -18,6 +18,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
+import java.util.HashMap;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -27,6 +28,9 @@ public class RegistryManagerIT
     private static String IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME = "IOTHUB_CONNECTION_STRING";
     private static String iotHubConnectionString = "";
     private static String deviceId = "java-crud-e2e-test";
+    private static String deviceForTest = "deviceForTest";
+    private static String moduleId = "java-crud-module-e2e-test";
+    private static String configId = "java-crud-adm-e2e-test";
     private static RegistryManager registryManager;
     private static final String primaryThumbprint =   "0000000000000000000000000000000000000000";
     private static final String secondaryThumbprint = "1111111111111111111111111111111111111111";
@@ -60,7 +64,7 @@ public class RegistryManagerIT
     }
 
     @Test
-    public void crud_e2e() throws Exception
+    public void crud_device_e2e() throws Exception
     {
         // Arrange
         deleteDeviceIfItExistsAlready(registryManager, deviceId);
@@ -88,7 +92,7 @@ public class RegistryManagerIT
     }
 
     @Test
-    public void crud_e2e_X509_CA_signed() throws Exception
+    public void crud_device_e2e_X509_CA_signed() throws Exception
     {
         // Arrange
         deleteDeviceIfItExistsAlready(registryManager, deviceId);
@@ -121,7 +125,7 @@ public class RegistryManagerIT
     }
 
     @Test
-    public void crud_e2e_X509_self_signed() throws Exception
+    public void crud_device_e2e_X509_self_signed() throws Exception
     {
         // Arrange
         deleteDeviceIfItExistsAlready(registryManager, deviceId);
@@ -161,9 +165,202 @@ public class RegistryManagerIT
     {
         RegistryManager registryManager = RegistryManager.createFromConnectionString(iotHubConnectionString);
         registryManager.getStatistics();
-
-        registryManager.close();
     }
+
+    @Test
+    public void crud_module_e2e() throws Exception
+    {
+        // Arrange
+        SymmetricKey expectedSymmetricKey = new SymmetricKey();
+
+        deleteDeviceIfItExistsAlready(registryManager, deviceForTest);
+        Device deviceSetup = Device.createFromId(deviceForTest, DeviceStatus.Enabled, null);
+        registryManager.addDevice(deviceSetup);
+        deleteModuleIfItExistsAlready(registryManager, deviceForTest, moduleId);
+
+        //-Create-//
+        Module moduleAdded = Module.createFromId(deviceForTest, moduleId, null);
+        registryManager.addModule(moduleAdded);
+
+        //-Read-//
+        Module moduleRetrieved = registryManager.getModule(deviceForTest, moduleId);
+
+        //-Update-//
+        Module moduleUpdated = registryManager.getModule(deviceForTest, moduleId);
+        moduleUpdated.getSymmetricKey().setPrimaryKey(expectedSymmetricKey.getPrimaryKey());
+        moduleUpdated = registryManager.updateModule(moduleUpdated);
+
+        //-Delete-//
+        registryManager.removeModule(deviceForTest, moduleId);
+        registryManager.removeDevice(deviceForTest);
+
+        // Assert
+        assertEquals(deviceForTest, moduleAdded.getDeviceId());
+        assertEquals(moduleId, moduleAdded.getId());
+        assertEquals(deviceForTest, moduleRetrieved.getDeviceId());
+        assertEquals(moduleId, moduleRetrieved.getId());
+        assertEquals(expectedSymmetricKey.getPrimaryKey(), moduleUpdated.getPrimaryKey());
+        assertTrue(moduleWasDeletedSuccessfully(registryManager, deviceForTest, moduleId));
+    }
+
+    @Test
+    public void crud_module_e2e_X509_CA_signed() throws Exception
+    {
+        // Arrange
+        deleteDeviceIfItExistsAlready(registryManager, deviceForTest);
+        deleteModuleIfItExistsAlready(registryManager, deviceForTest, moduleId);
+        Device deviceSetup = Device.createFromId(deviceForTest, DeviceStatus.Enabled, null);
+        registryManager.addDevice(deviceSetup);
+        deleteModuleIfItExistsAlready(registryManager, deviceForTest, moduleId);
+
+        //-Create-//
+        Module moduleAdded = Module.createModule(deviceForTest, moduleId, AuthenticationType.CERTIFICATE_AUTHORITY);
+        registryManager.addModule(moduleAdded);
+
+        //-Read-//
+        Module moduleRetrieved = registryManager.getModule(deviceForTest, moduleId);
+
+        //-Delete-//
+        registryManager.removeModule(deviceForTest, moduleId);
+        registryManager.removeDevice(deviceForTest);
+
+        // Assert
+        assertEquals(deviceForTest, moduleAdded.getDeviceId());
+        assertEquals(moduleId, moduleAdded.getId());
+        assertEquals(deviceForTest, moduleRetrieved.getDeviceId());
+        assertEquals(moduleId, moduleRetrieved.getId());
+        assertNull(moduleAdded.getPrimaryThumbprint());
+        assertNull(moduleAdded.getSecondaryThumbprint());
+        assertNull(moduleRetrieved.getPrimaryThumbprint());
+        assertNull(moduleRetrieved.getSecondaryThumbprint());
+        assertTrue(moduleWasDeletedSuccessfully(registryManager, deviceForTest, moduleId));
+    }
+
+    @Test
+    public void crud_module_e2e_X509_self_signed() throws Exception
+    {
+        // Arrange
+        deleteDeviceIfItExistsAlready(registryManager, deviceForTest);
+        Device deviceSetup = Device.createFromId(deviceForTest, DeviceStatus.Enabled, null);
+        registryManager.addDevice(deviceSetup);
+        deleteModuleIfItExistsAlready(registryManager, deviceForTest, moduleId);
+
+        //-Create-//
+        Module moduleAdded = Module.createModule(deviceForTest, moduleId, AuthenticationType.SELF_SIGNED);
+        moduleAdded.setThumbprint(primaryThumbprint, secondaryThumbprint);
+        registryManager.addModule(moduleAdded);
+
+        //-Read-//
+        Module moduleRetrieved = registryManager.getModule(deviceForTest, moduleId);
+
+        //-Update-//
+        Module moduleUpdated = registryManager.getModule(deviceForTest, moduleId);
+        moduleUpdated.setThumbprint(primaryThumbprint2, secondaryThumbprint2);
+        moduleUpdated = registryManager.updateModule(moduleUpdated);
+
+        //-Delete-//
+        registryManager.removeModule(deviceForTest, moduleId);
+
+        // Assert
+        assertEquals(deviceForTest, moduleAdded.getDeviceId());
+        assertEquals(moduleId, moduleAdded.getId());
+        assertEquals(deviceForTest, moduleRetrieved.getDeviceId());
+        assertEquals(moduleId, moduleRetrieved.getId());
+        assertEquals(AuthenticationType.SELF_SIGNED, moduleAdded.getAuthenticationType());
+        assertEquals(AuthenticationType.SELF_SIGNED, moduleRetrieved.getAuthenticationType());
+        assertEquals(primaryThumbprint, moduleAdded.getPrimaryThumbprint());
+        assertEquals(secondaryThumbprint, moduleAdded.getSecondaryThumbprint());
+        assertEquals(primaryThumbprint, moduleRetrieved.getPrimaryThumbprint());
+        assertEquals(secondaryThumbprint, moduleRetrieved.getSecondaryThumbprint());
+        assertEquals(primaryThumbprint2, moduleUpdated.getPrimaryThumbprint());
+        assertEquals(secondaryThumbprint2, moduleUpdated.getSecondaryThumbprint());
+        assertTrue(moduleWasDeletedSuccessfully(registryManager, deviceId, moduleId));
+    }
+
+    @Test
+    public void crud_adm_configuration_e2e() throws Exception
+    {
+        // Arrange
+        deleteConfigurationIfItExistsAlready(registryManager, deviceId);
+        final HashMap<String, Object> testDeviceContent = new HashMap<String, Object>()
+        {
+            {
+                put("properties.desired.chiller-water", new HashMap<String, Object>()
+                        {
+                            {
+                                put("temperature", 66);
+                                put("pressure", 28);
+                            }
+                        }
+                );
+            }
+        };
+
+        //-Create-//
+        Configuration configAdded = new Configuration(configId);
+
+        ConfigurationContent content = new ConfigurationContent();
+        content.setDeviceContent(testDeviceContent);
+        configAdded.setContent(content);
+        configAdded.getMetrics().setQueries(new HashMap<String, String>(){{put("waterSettingsPending",
+                "SELECT deviceId FROM devices WHERE properties.reported.chillerWaterSettings.status=\'pending\'");}});
+        configAdded.setTargetCondition("properties.reported.chillerProperties.model=\'4000x\'");
+        configAdded.setPriority(20);
+        registryManager.addConfiguration(configAdded);
+
+        //-Read-//
+        Configuration configRetrieved = registryManager.getConfiguration(configId);
+
+        //-Update-//
+        Configuration configUpdated = registryManager.getConfiguration(configId);
+        configUpdated.setPriority(1);
+        configUpdated = registryManager.updateConfiguration(configUpdated);
+
+        //-Delete-//
+        registryManager.removeConfiguration(configId);
+
+        // Assert
+        assertEquals(configId, configAdded.getId());
+        assertEquals(configId, configRetrieved.getId());
+        assertEquals("{temperature=66.0, pressure=28.0}",
+                configRetrieved.getContent().getDeviceContent().get("properties.desired.chiller-water").toString());
+        assertEquals("SELECT deviceId FROM devices WHERE properties.reported.chillerWaterSettings.status=\'pending\'",
+                configRetrieved.getMetrics().getQueries().get("waterSettingsPending"));
+        assertEquals("properties.reported.chillerProperties.model=\'4000x\'",
+                configRetrieved.getTargetCondition());
+        assertEquals(new Integer(20), configRetrieved.getPriority());
+        assertEquals(configId, configUpdated.getId());
+        assertEquals(new Integer(1), configUpdated.getPriority());
+        assertTrue(configWasDeletedSuccessfully(registryManager, configId));
+    }
+
+    @Test(expected = IotHubBadFormatException.class)
+    public void apply_configuration_e2e() throws Exception
+    {
+        // Arrange
+        deleteDeviceIfItExistsAlready(registryManager, deviceForTest);
+        Device deviceSetup = Device.createFromId(deviceForTest, DeviceStatus.Enabled, null);
+        registryManager.addDevice(deviceSetup);
+        final HashMap<String, Object> testDeviceContent = new HashMap<String, Object>()
+        {
+            {
+                put("properties.desired.chiller-water", new HashMap<String, Object>()
+                        {
+                            {
+                                put("temperature", 66);
+                                put("pressure", 28);
+                            }
+                        }
+                );
+            }
+        };
+        ConfigurationContent content = new ConfigurationContent();
+        content.setDeviceContent(testDeviceContent);
+
+        // Act
+        registryManager.applyConfigurationContentOnDevice(deviceForTest, content);
+    }
+
 
     private void deleteDeviceIfItExistsAlready(RegistryManager registryManager, String deviceId) throws IOException
     {
@@ -186,6 +383,48 @@ public class RegistryManagerIT
         }
     }
 
+    private void deleteModuleIfItExistsAlready(RegistryManager registryManager, String deviceId, String moduleId) throws IOException
+    {
+        try
+        {
+            registryManager.getModule(deviceId, moduleId);
+
+            //if no exception yet, device exists so it can be deleted
+            try
+            {
+                registryManager.removeModule(deviceId, moduleId);
+            }
+            catch (IotHubException | IOException e)
+            {
+                System.out.println("Initialization failed, could not remove deviceId/moduleId: " + deviceId + "/" + moduleId);
+            }
+        }
+        catch (IotHubException e)
+        {
+        }
+    }
+
+    private void deleteConfigurationIfItExistsAlready(RegistryManager registryManager, String configId) throws IOException
+    {
+        try
+        {
+            registryManager.getConfiguration(configId);
+
+            //if no exception yet, device exists so it can be deleted
+            try
+            {
+                registryManager.removeConfiguration(configId);
+            }
+            catch (IotHubException | IOException e)
+            {
+                System.out.println("Initialization failed, could not remove configuration" + configId);
+            }
+        }
+        catch (IotHubException e)
+        {
+        }
+    }
+
     private boolean deviceWasDeletedSuccessfully(RegistryManager registryManager, String deviceId) throws IOException
     {
         try
@@ -199,6 +438,38 @@ public class RegistryManagerIT
         }
 
         // device could still be retrieved, so it was not deleted successfully
+        return false;
+    }
+
+    private boolean moduleWasDeletedSuccessfully(RegistryManager registryManager, String deviceId, String moduleId) throws IOException
+    {
+        try
+        {
+            registryManager.getModule(deviceId, moduleId);
+        }
+        catch (IotHubException e)
+        {
+            // module should have been deleted, so this catch is expected
+            return true;
+        }
+
+        // module could still be retrieved, so it was not deleted successfully
+        return false;
+    }
+
+    private boolean configWasDeletedSuccessfully(RegistryManager registryManager, String configId) throws IOException
+    {
+        try
+        {
+            registryManager.getConfiguration(configId);
+        }
+        catch (IotHubException e)
+        {
+            // configuration should have been deleted, so this catch is expected
+            return true;
+        }
+
+        // configuration could still be retrieved, so it was not deleted successfully
         return false;
     }
 }
