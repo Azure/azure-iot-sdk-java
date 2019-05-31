95913b89dde724794ec43fae61fd939c8f1edf97
timtay-microsoft <timtay@microsoft.com>
2019-05-30 22:13:50 -0700

    fix(iot-dev):Fix amqp layer not handling proton:io exceptions
    Server causes this error type to appear during TCP connection lost tests, so it must be a retryable exception

---
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/amqps/exceptions/ProtonIOException.java
@@ -0,0 +1,32 @@
+package com.microsoft.azure.sdk.iot.device.transport.amqps.exceptions;
+
+import com.microsoft.azure.sdk.iot.device.exceptions.ProtocolException;
+
+public class ProtonIOException extends ProtocolException
+{
+    public static final String errorCode = "proton:io";
+
+    public ProtonIOException()
+    {
+        super();
+        this.isRetryable = true;
+    }
+
+    public ProtonIOException(String message)
+    {
+        super(message);
+        this.isRetryable = true;
+    }
+
+    public ProtonIOException(String message, Throwable cause)
+    {
+        super(message, cause);
+        this.isRetryable = true;
+    }
+
+    public ProtonIOException(Throwable cause)
+    {
+        super(cause);
+        this.isRetryable = true;
+    }
+}
