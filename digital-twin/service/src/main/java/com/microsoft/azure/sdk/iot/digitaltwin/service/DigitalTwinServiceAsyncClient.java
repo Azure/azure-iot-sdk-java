2b491d01c15eac39338f2054d2f3aff23ff4874e
Abhipsa Misra <abmisr@microsoft.com>
2019-08-28 18:24:28 -0700

    feat(digitaltwin-service): Return T instead of Response<T>

--- digital-twin/service/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/service/DigitalTwinServiceAsyncClient.java
+++ digital-twin/service/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/service/DigitalTwinServiceAsyncClient.java
@@ -3,8 +3,6 @@
 
 package com.microsoft.azure.sdk.iot.digitaltwin.service;
 
-import com.azure.core.http.rest.Response;
-import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.implementation.annotation.ReturnType;
 import com.azure.core.implementation.annotation.ServiceClient;
 import com.azure.core.implementation.annotation.ServiceMethod;
@@ -34,50 +32,36 @@ public final class DigitalTwinServiceAsyncClient {
     /**
      * Retrieves the state of a single digital twin
      * @param digitalTwinId The ID of the digital twin. Format of digitalTwinId is DeviceId[~ModuleId]. ModuleId is optional
-     * @return The observable to the response containing the state of the full digital twin, including all properties
-     * of all components registered by that digital twin
+     * @return The observable to the state of the full digital twin, including all properties of all components registered by that digital twin
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Observable<Response<DigitalTwin>> getDigitalTwin(String digitalTwinId) {
+    public Observable<DigitalTwin> getDigitalTwin(String digitalTwinId) {
         return this.digitalTwins.getInterfacesAsync(digitalTwinId)
-                .map(digitalTwinInterfaces -> new SimpleResponse<DigitalTwin>(null, new DigitalTwin(digitalTwinInterfaces)));
+                .map(DigitalTwin ::new);
     }
 
     /**
-     * Retrieves the response containing the response headers and the interface of given interfaceName
-     * Example URI: "digitalTwins/{digitalTwinId}/interfaces/{interfaceName}"
-     * @param digitalTwinId The ID of the digital twin. Format of digitalTwinId is DeviceId[~ModuleId]. ModuleId is optional
-     * @param interfaceName The interface name
-     * @return The observable to the response containing the the interface of given interfaceName
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    public Observable<Response<DigitalTwin>> getDigitalTwinInterface(String digitalTwinId, String interfaceName) {
-        return this.digitalTwins.getInterfaceAsync(digitalTwinId, interfaceName)
-                .map(digitalTwinInterface -> new SimpleResponse<DigitalTwin>(null, new DigitalTwin(digitalTwinInterface)));
-    }
-
-    /**
-     * Retrieve the response containing the DigitalTwin model definition for the given id
+     * Retrieve the DigitalTwin model definition for the given id
      * @param modelId The model ID. Ex: &lt;example&gt;urn:contoso:TemperatureSensor:1&lt;/example&gt;
-     * @return The observable to the response containing the DigitalTwin model definition
+     * @return The observable to the DigitalTwin model definition
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Observable<Response<String>> getModel(String modelId) {
+    public Observable<String> getModel(String modelId) {
         return this.digitalTwins.getDigitalTwinModelAsync(modelId)
-                .map(digitalTwinModel -> new SimpleResponse<String>(null, String.valueOf(digitalTwinModel)));
+                .map(String :: valueOf);
     }
 
     /**
-     * Retrieve the response containing the DigitalTwin model definition for the given id
+     * Retrieve the DigitalTwin model definition for the given id
      * @param modelId The model ID. Ex: &lt;example&gt;urn:contoso:TemperatureSensor:1&lt;/example&gt;
      * @param expand Indicates whether to expand the device capability model's interface definitions inline or not.
      *               This query parameter ONLY applies to Capability model.
-     * @return The observable to the response containing the DigitalTwin model definition
+     * @return The observable to the DigitalTwin model definition
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Observable<Response<String>> getModel(String modelId, Boolean expand) {
+    public Observable<String> getModel(String modelId, Boolean expand) {
         return this.digitalTwins.getDigitalTwinModelAsync(modelId, expand)
-                .map(digitalTwinModel -> new SimpleResponse<String>(null, String.valueOf(digitalTwinModel)));
+                .map(String :: valueOf);
     }
 
 }
