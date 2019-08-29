064ea4309dcc8e938b7d360ddb00c73933c0cbf5
Abhipsa Misra <abmisr@microsoft.com>
2019-08-28 18:24:28 -0700

    feat(digitaltwin-service): Return T instead of Response<T>

--- digital-twin/service/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/service/DigitalTwinServiceClient.java
+++ digital-twin/service/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/service/DigitalTwinServiceClient.java
@@ -3,8 +3,6 @@
 
 package com.microsoft.azure.sdk.iot.digitaltwin.service;
 
-import com.azure.core.http.rest.Response;
-import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.implementation.annotation.ReturnType;
 import com.azure.core.implementation.annotation.ServiceClient;
 import com.azure.core.implementation.annotation.ServiceMethod;
@@ -29,48 +27,33 @@ public final class DigitalTwinServiceClient {
     /**
      * Retrieves the state of a single digital twin
      * @param digitalTwinId The ID of the digital twin. Format of digitalTwinId is DeviceId[~ModuleId]. ModuleId is optional
-     * @return The response containing the state of the full digital twin, including all properties
-     * of all components registered by that digital twin
+     * @return The state of the full digital twin, including all properties of all components registered by that digital twin
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<DigitalTwin> getDigitalTwin(String digitalTwinId) {
-        DigitalTwin digitalTwin = new DigitalTwin(this.digitalTwins.getInterfaces(digitalTwinId));
-        return new SimpleResponse<DigitalTwin>(null, digitalTwin);
+    public DigitalTwin getDigitalTwin(String digitalTwinId) {
+        return new DigitalTwin(this.digitalTwins.getInterfaces(digitalTwinId));
     }
 
     /**
-     * Retrieves the response containing the response headers and the interface of given interfaceName
-     * Example URI: "digitalTwins/{digitalTwinId}/interfaces/{interfaceName}"
-     * @param digitalTwinId The ID of the digital twin. Format of digitalTwinId is DeviceId[~ModuleId]. ModuleId is optional
-     * @param interfaceName The interface name
-     * @return The response containing the the interface of given interfaceName
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<DigitalTwin> getDigitalTwinInterface(String digitalTwinId, String interfaceName) {
-        DigitalTwin digitalTwin = new DigitalTwin(this.digitalTwins.getInterface(digitalTwinId, interfaceName));
-        return new SimpleResponse<DigitalTwin>(null, digitalTwin);
-    }
-
-    /**
-     * Retrieve the response containing the DigitalTwin model definition for the given id
+     * Retrieve the DigitalTwin model definition for the given id
      * @param modelId The model ID. Ex: &lt;example&gt;urn:contoso:TemperatureSensor:1&lt;/example&gt;
-     * @return The response containing the DigitalTwin model definition
+     * @return The DigitalTwin model definition
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<String> getModel(String modelId) {
-        return new SimpleResponse<String>(null, String.valueOf(this.digitalTwins.getDigitalTwinModel(modelId)));
+    public String getModel(String modelId) {
+        return String.valueOf(this.digitalTwins.getDigitalTwinModel(modelId));
     }
 
     /**
-     * Retrieve the response containing the DigitalTwin model definition for the given id
+     * Retrieve the DigitalTwin model definition for the given id
      * @param modelId The model ID. Ex: &lt;example&gt;urn:contoso:TemperatureSensor:1&lt;/example&gt;
      * @param expand Indicates whether to expand the device capability model's interface definitions inline or not.
      *               This query parameter ONLY applies to Capability model.
-     * @return The response containing the DigitalTwin model definition
+     * @return The DigitalTwin model definition
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<String> getModel(String modelId, Boolean expand) {
-        return new SimpleResponse<String>(null, String.valueOf(this.digitalTwins.getDigitalTwinModel(modelId, expand)));
+    public String getModel(String modelId, Boolean expand) {
+        return String.valueOf(this.digitalTwins.getDigitalTwinModel(modelId, expand));
     }
 
 }
