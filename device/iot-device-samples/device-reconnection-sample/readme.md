98b0b268c9e3bfae82b0e3494c6405f9af1995fc
Abhipsa Misra <abmisr@microsoft.com>
2019-10-17 13:42:09 -0700

    feat(samples): Add sample that demonstrates device client reconnection

---
+++ device/iot-device-samples/device-reconnection-sample/readme.md
@@ -0,0 +1,56 @@
+# Device Reconnection Sample
+
+This sample code demonstrates the various connection status changes and connection status change reasons the Device Client can return, and how to handle them.
+
+The device client exhibits the following connection status changes with reason:
+
+<table>
+  <tr>
+    <th> Connection Status </th>
+    <th> Change Reason </th>
+    <th> Ownership of connectivity </th>
+    <th> Comments </th>
+  </tr>
+  <tr>
+    <td> CONNECTED </td>
+    <td> CONNECTED_OK </td>
+    <td> SDK </td>
+    <td> SDK tries to remain connected to the service and can carry out all operation as normal </td>
+  </tr>
+  <tr>
+    <td rowspan="2"> DISCONNECTED_RETRYING </td>
+    <td> NO_NETWORK </td>
+    <td rowspan="2"> SDK </td>
+    <td rowspan="2"> When disconnection happens because of any reason (network failures, transient loss of connectivity etc.), SDK makes best attempt to connect back to IotHub. The RetryPolicy applied on the DeviceClient will be used to determine the count of reconnection attempts for <em>retriable</em> errors </td>
+  </tr>
+  <tr>
+    <td> COMMUNICATION_ERROR </td>
+  </tr>
+  <tr>
+    <td rowspan="5"> DISCONNECTED </td>
+    <td> CLIENT_CLOSE </td>
+    <td rowspan="5"> Application </td>
+    <td> This is state when SDK was asked to close the connection by application </td>
+  </tr>
+  <tr>
+    <td> BAD_CREDENTIAL </td>
+    <td> Supplied credential isn’t good for device to connect to service - Fix the supplied credentials before attempting to reconnect again </td>
+  </tr>
+  <tr>
+    <td> EXPIRED_SAS_TOKEN </td>
+    <td> Supplied SAS Token has expired - Fix the supplied SAS token before attempting to reconnect again </td>
+  </tr>
+  <tr>
+    <td> COMMUNICATION_ERROR </td>
+    <td> This is the state when SDK landed up in a non-retriable error during communication - Inspect the <em>throwable</em> supplied in the <code>IotHubConnectionStatusChangeCallback</code> to get the stacktrace of the exception </td>
+  </tr>
+  <tr>
+    <td> RETRY_EXPIRED </td>
+    <td> This is the state when SDK attempted maximum retries set by the application </td>
+  </tr>
+</table>
+
+For status `CLIENT_CLOSE`, `COMMUNICATION_ERROR` and `RETRY_EXPIRED`, the application can attempt to reconnect by closing and opening the client again eg. calling `deviceClient.closeNow();` `deviceClient.open();` in a separate thread.
+
+We would like to highlight here that the thread used to deliver the `IotHubConnectionStatusChangeCallback` should not be used to call `open()`/`closeNow()` on the client that this callback belongs to. All open()/closeNow() operations should be done on a separate thread.
+
---
+++ device/iot-device-samples/device-reconnection-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/Application.java
@@ -0,0 +1,168 @@
+// Copyright (c) Microsoft. All rights reserved.Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+package samples.com.microsoft.azure.sdk.iot;
+
+import com.microsoft.azure.sdk.iot.device.DeviceClient;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.Pair;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.Property;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.TwinPropertyCallBack;
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
+import com.microsoft.azure.sdk.iot.device.Message;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static samples.com.microsoft.azure.sdk.iot.DeviceTwinHelper.*;
+import static samples.com.microsoft.azure.sdk.iot.MethodHelper.DeviceMethodStatusCallBack;
+import static samples.com.microsoft.azure.sdk.iot.MethodHelper.SampleDeviceMethodCallback;
+import static samples.com.microsoft.azure.sdk.iot.ReceiveMessageHelper.MessageCallback;
+import static samples.com.microsoft.azure.sdk.iot.TelemetryHelper.EventCallback;
+import static samples.com.microsoft.azure.sdk.iot.TelemetryHelper.composeMessage;
+
+@Slf4j
+public class Application {
+    private static final String deviceConnectionString = "<FILL_IN_DEVICE_CONNECTION_STRING_HERE>";
+    private static final IotHubClientProtocol protocol = IotHubClientProtocol.AMQPS;
+    private static final int DEVICE_OPERATION_TIMEOUT_IN_MINUTES = 2;
+    private static final int numRequests = 20;
+    private static final int MAX_APPLICATION_RUN_DURATION_IN_MINUTES = 5;
+    private static final int SLEEP_DURATION_IN_SECS = 30;
+    private static AtomicBoolean stopRunning = new AtomicBoolean(false);
+
+    static boolean reconnectIndefinitely = true;
+    static List<String> failedMessageListOnClose = new ArrayList<>(); // List of messages that failed on close
+    private static CountDownLatch countDownLatch = new CountDownLatch(2);
+    private static DeviceClientManager deviceClientManager;
+
+    /**
+     * Sends a number of messages to an IoT. Default protocol is to use AMQP transport.
+     *
+     */
+    public static void main(String[] args)
+            throws IOException, URISyntaxException, InterruptedException {
+        log.debug("Starting the sample...");
+        log.debug("Using communication protocol: {}", protocol.name());
+
+        DeviceClient client = new DeviceClient(deviceConnectionString, protocol);
+        log.debug("Successfully created an IoT Hub client.");
+
+        deviceClientManager = new DeviceClientManager(client);
+        deviceClientManager.setOperationTimeout(DEVICE_OPERATION_TIMEOUT_IN_MINUTES);
+
+        deviceClientManager.connect();
+        log.debug("Opened connection to IoT Hub.");
+
+        log.debug("Setting C2D message handler...");
+        deviceClientManager.setMessageCallback(new MessageCallback(), new Object());
+
+        log.debug("Setting method handler...");
+        deviceClientManager.subscribeToDeviceMethod(new SampleDeviceMethodCallback(), null, new DeviceMethodStatusCallBack(), null);
+
+        System.out.println("Start device Twin and get remaining properties...");
+        // Properties already set in the Service will shows up in the generic onProperty callback, with value and version.
+        Succeed.set(false);
+        client.startDeviceTwin(new DeviceTwinStatusCallBack(), null, new onProperty(), null);
+        do
+        {
+            Thread.sleep(1000);
+        }
+        while(!Succeed.get());
+
+        log.debug("Subscribe to Desired properties on device Twin: PropertyKey={}...", TwinPropertyKey);
+        Map<Property, Pair<TwinPropertyCallBack, Object>> propertyPairMap =
+                singletonMap(new Property(TwinPropertyKey, null), new Pair<TwinPropertyCallBack, Object>(new onProperty(), null));
+        deviceClientManager.subscribeToTwinDesiredProperties(propertyPairMap);
+
+        log.debug("Start sending telemetry and reported property updates...");
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                updateReportedProperties();
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                sendTelemetry();
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        countDownLatch.await(MAX_APPLICATION_RUN_DURATION_IN_MINUTES, TimeUnit.MINUTES);
+        stopRunning.set(true);
+        log.debug("Cancelling reported property updates and telemetry operation, application is ready to shutdown...");
+
+        // close the connection
+        log.debug("Closing");
+        deviceClientManager.disconnect();
+
+        if (! failedMessageListOnClose.isEmpty()) {
+            log.debug("List of messages that were cancelled on close: {}", failedMessageListOnClose.toString());
+        }
+
+        log.debug("Shutting down...");
+    }
+
+    private static void updateReportedProperties() {
+        log.debug("Updating reported properties...");
+        for (int i = 0; i < numRequests; ++ i) {
+            if (stopRunning.get()) break;
+
+            int nextValue = ThreadLocalRandom.current().nextInt(70, 90);
+            Set<Property> reportProperties = new HashSet<>(singletonList(new Property(TwinPropertyKey, nextValue)));
+            try {
+                deviceClientManager.sendReportedProperties(reportProperties);
+                log.debug(">> Updating reported properties: {}={}...", TwinPropertyKey, nextValue);
+                log.debug("Get device Twin...");
+                deviceClientManager.getDeviceTwin();
+            }
+            catch (IOException e) {
+                log.error("Error occurred while updating reported property...");
+                e.printStackTrace();
+            }
+            try {
+                log.debug("Sleeping for {} secs before sending the next property update.", SLEEP_DURATION_IN_SECS);
+                Thread.sleep(SLEEP_DURATION_IN_SECS * 1000);
+            } catch (InterruptedException e) {
+                log.error("Exception thrown while sleeping...");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void sendTelemetry() {
+        log.debug("Sending the following event messages:");
+        for (int i = 0; i < numRequests; ++ i) {
+            if (stopRunning.get()) break;
+
+            try {
+                Message msg = composeMessage(i);
+                EventCallback callback = new EventCallback();
+                deviceClientManager.sendEventAsync(msg, callback, msg);
+            }
+            catch (Exception e) {
+                log.error("Exception thrown while sending telemetry...");
+                e.printStackTrace();
+            }
+            try {
+                log.debug("Sleeping for {} secs before sending next message.", SLEEP_DURATION_IN_SECS);
+                Thread.sleep(SLEEP_DURATION_IN_SECS * 1000);
+            } catch (InterruptedException e) {
+                log.error("Exception thrown while sleeping...");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
