3ee4a5e93ca24edecf0e0bc16aa56c42d6422959
timtay-microsoft <timtay@microsoft.com>
2019-11-19 15:23:29 -0800

    fix(iot-serv, iot-dev): Remove SDK side validation of reported and desired properties
    $ is used in digital twin communication, so the device client should not do this validation anymore. Service client is updated to reflect that too.

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/devicetwin/Pair.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/devicetwin/Pair.java
@@ -21,13 +21,6 @@ public class Pair
              */
             throw new IllegalArgumentException("Key cannot be null or empty");
         }
-        if (key.contains(" ") || key.contains("$") || key.contains(".") || key.length() > MAX_ALLOWABLE_KEY_LENGTH)
-        {
-            /*
-            **Codes_SRS_Pair_25_003: [**If the key contains illegal unicode control characters i.e ' ', '.', '$' or if length is greater than 124 chars, the constructor shall throw an IllegalArgumentException.**]**
-             */
-            throw new IllegalArgumentException("Key cannot contain illegal unicode control characters '.', '$', ' '");
-        }
         /*
         **Codes_SRS_Pair_25_001: [**The constructor shall save the key and value representing this Pair.**]**
          */
