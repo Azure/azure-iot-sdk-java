3b43d578e67bb98862caec5560003bdb2053091a
timtay-microsoft <timtay@microsoft.com>
2019-11-20 13:42:09 -0800

    refactor(iot-dev): Update API version to preview

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/TransportUtils.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/TransportUtils.java
@@ -7,7 +7,7 @@ import com.microsoft.azure.sdk.iot.device.exceptions.TransportException;
 
 public class TransportUtils
 {
-    public static String IOTHUB_API_VERSION = "2018-06-30";
+    public static String IOTHUB_API_VERSION = "2019-07-01-preview";
 
     private static final String JAVA_DEVICE_CLIENT_IDENTIFIER = "com.microsoft.azure.sdk.iot.iot-device-client";
     private static final String CLIENT_VERSION = "1.18.0";
