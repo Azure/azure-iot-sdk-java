0cbd68bf0b5f482be9c36f7e3c4bf829e8f44e35
timtay-microsoft <timtay@microsoft.com>
2019-12-11 11:39:40 -0800

    fix(vsts): Fix vsts pipelines
    Consolidated to one storage account connection string keyvault key since they all shared the same value
    
    Moved DPS global endpoint out from keyvault since it is not a secret
    
    Removed some old yaml and scripts that were not being used
    
    Upgraded android gradle plugin version to fix android build

---
+++ vsts/android_java.cmd
@@ -0,0 +1,21 @@
+@REM Copyright (c) Microsoft. All rights reserved.
+@REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+setlocal
+
+set build-root=%~dp0..
+@REM // resolve to fully qualified path
+for %%i in ("%build-root%") do set build-root=%%~fi
+
+@REM -- Delete m2 folder--
+
+call RD /S /Q "c:/users/%USERNAME%/.m2/repository/com/microsoft/azure/sdk/iot"
+
+@REM -- Android Test Build --
+cd %build-root%
+call mvn clean install -DskipTests=true -T 2C
+if errorlevel 1 goto :eof
+cd %build-root%\iot-e2e-tests\android
+call gradle wrapper
+call gradlew :clean :app:clean :app:assembleDebug
+call gradlew :app:assembleDebugAndroidTest -PIotHubConnectionString=%IOTHUB_CONNECTION_STRING% -PIotHubInvalidCertConnectionString=%IOTHUB_CONN_STRING_INVALIDCERT% -PAppCenterAppSecret=%APPCENTER_APP_SECRET% -PDeviceProvisioningServiceConnectionString=%IOT_DPS_CONNECTION_STRING% -PDeviceProvisioningServiceIdScope=%DEVICE_PROVISIONING_SERVICE_ID_SCOPE% -PInvalidDeviceProvisioningServiceGlobalEndpoint=%INVALID_DEVICE_PROVISIONING_SERVICE_GLOBAL_ENDPOINT% -PInvalidDeviceProvisioningServiceConnectionString=%INVALID_DEVICE_PROVISIONING_SERVICE_CONNECTION_STRING% -PFarAwayIotHubConnectionString=%FAR_AWAY_IOTHUB_CONNECTION_STRING% -PCustomAllocationWebhookUrl=%CUSTOM_ALLOCATION_POLICY_WEBHOOK% -PIsBasicTierHub=%IS_BASIC_TIER_HUB%
