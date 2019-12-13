a2d07402c44414fee6b01ba6139982eade9ee1ac
Abhipsa Misra <abmisr@microsoft.com>
2019-12-12 16:12:20 -0800

    fix(e2e-tests): Updated the environment variable

--- digital-twin/e2e-tests/iot-digitaltwin-e2e-tests/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/e2e/helpers/E2ETestConstants.java
+++ digital-twin/e2e-tests/iot-digitaltwin-e2e-tests/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/e2e/helpers/E2ETestConstants.java
@@ -8,6 +8,6 @@ public class E2ETestConstants {
     public static final int MAX_WAIT_TIME_FOR_ASYNC_CALL_IN_SECONDS = 5;
     public static final String DCM_ID = "urn:contoso:azureiot:sdk:testinterface:cm:2";
 
-    public static final String IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME = "IOT_HUB_CONNECTION_STRING";
+    public static final String IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME = "IOTHUB_CONNECTION_STRING";
     public static final String EVENT_HUB_CONNECTION_STRING_VAR_NAME = "EVENT_HUB_CONNECTION_STRING";
 }
