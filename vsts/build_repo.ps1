5f4a9b79016d2e761f592017cafe995907e9b77a
timtay-microsoft <timtay@microsoft.com>
2020-01-08 11:19:53 -0800

    asdf

--- vsts/build_repo.ps1
+++ vsts/build_repo.ps1
@@ -1,1 +1,3 @@
-﻿mvn install -DskipITs=false -T 2C "-Dfailsafe.rerunFailingTestsCount=2" #parallelized to use 2 threads per core
+﻿./vsts/determine_tests_to_run.ps1
+
+mvn -DRUN_PROVISIONING_TESTS=$Env:runProvisioningTests -DRUN_IOTHUB_TESTS=$Env:runIotHubTests install -DskipITs=false -T 2C "-Dfailsafe.rerunFailingTestsCount=2" #parallelized to use 2 threads per core
---
+++ vsts/determine_tests_to_run.ps1
@@ -0,0 +1,97 @@
+﻿# For pull requests, this script checks the git diff between the merging branch and the upstream branch to see
+#  if any code changes were made to iothub clients or provisioning clients. As a result, this script sets environment
+#  variables to signal later vsts scripts to run or not to run certain e2e tests
+
+# For all other builds (nightly builds, for instance), this script will set environment variables to run all e2e tests.
+
+Write-Host "Determining tests to run..."
+$Env:runIotHubTests = "false"
+$Env:runProvisioningTests = "false"
+
+$targetBranch = ($env:TARGET_BRANCH)
+if (($env:TARGET_BRANCH).Contains("System.PullRequest.TargetBranch"))
+{
+    Write-Host "Assuming this build is not a pull request build, running all tests"
+    $targetBranch = "master"
+
+    # TODO test this path
+    $Env:runIotHubTests = "true"
+    $Env:runProvisioningTests = "true"
+
+    # TODO Temp hardcode, remove these
+    Write-Host "Setting runIotHubTests to false"
+    $Env:runIotHubTests = "false"
+
+    Write-Host "Setting runProvisioningTests to true"
+    $Env:runProvisioningTests = "true"
+
+    exit 0
+}
+
+$GitDiff = & git diff origin/$targetBranch --name-only
+ForEach ($line in $($GitDiff -split "`r`n"))
+{
+	if ($line.EndsWith(".md", "CurrentCultureIgnoreCase") -or $line.EndsWith(".png", "CurrentCultureIgnoreCase"))
+	{
+        # These file types are ignored when determining if source code changes require running e2e tests
+	}
+	else
+	{
+	    # If code changes were made to provisioning sdk code or to provisioning e2e tests
+		if ($line.Contains("Provisioning", "CurrentCultureIgnoreCase"))
+		{
+			$Env:runProvisioningTests = "true"
+		}
+
+        # If code changes were made to iot hub e2e tests
+		if ($line.Contains("iothub", "CurrentCultureIgnoreCase"))
+		{
+			$Env:runIotHubTests = "true"
+		}
+
+		# If code changes were made to device client
+		if ($line.Contains("iot-device-client/src/main", "CurrentCultureIgnoreCase"))
+   		{
+   			$Env:runIotHubTests = "true"
+   		}
+
+        # If code changes were made to service client
+        if ($line.Contains("iot-service-client/src/main", "CurrentCultureIgnoreCase"))
+        {
+           	$Env:runIotHubTests = "true"
+        }
+
+        # Both provisioning and iot hub depend on deps package
+		if ($line.Contains("deps/src/main", "CurrentCultureIgnoreCase"))
+		{
+			$Env:runIotHubTests = "true"
+			$Env:runProvisioningTests = "true"
+		}
+
+        # Helpers can be used in any test, so we must run every test
+		if ($line.Contains("iot-e2e-tests/common/helpers", "CurrentCultureIgnoreCase"))
+        {
+           	$Env:runIotHubTests = "true"
+            $Env:runProvisioningTests = "true"
+        }
+	}
+}
+
+if ($Env:runIotHubTests -eq "true")
+{
+    Write-Host "Will run iot hub tests"
+}
+else
+{
+    Write-Host "Will not run iot hub tests"
+}
+
+if ($Env:runProvisioningTests -eq "true")
+{
+    Write-Host "Will run provisioning tests"
+}
+else
+{
+    Write-Host "Will not run provisioning tests"
+}
+
