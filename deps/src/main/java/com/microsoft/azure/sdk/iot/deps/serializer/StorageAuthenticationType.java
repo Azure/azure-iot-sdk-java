dfbc8a03a719d5ec2ddcfd951322e0684209350d
bikamani <41314966+bikamani@users.noreply.github.com>
2020-02-28 14:56:48 -0800

    beautifying code

--- deps/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/StorageAuthenticationType.java
+++ deps/src/main/java/com/microsoft/azure/sdk/iot/deps/serializer/StorageAuthenticationType.java
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName;
 /* Specifies authentication type being used for connecting to storage account */
 public enum StorageAuthenticationType
 {
-    /* Use a shared access key for authentication */
+    /** Use a shared access key for authentication */
     @SerializedName("keyBased")
     KEY,
 
-    /* Use the AD identity configured on the hub for authentication to storage */
+    /** Use the Active Directory identity configured on the hub for authentication to storage */
     @SerializedName("identityBased")
     IDENTITY,
 }
