beb4551dddb1f88e67ec606ced5150cb803a297d
timtay-microsoft <timtay@microsoft.com>
2020-05-18 12:25:23 -0700

    doc(iot-dev): Add missing javadoc explaining SetSendInterval for setOption API

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/InternalClient.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/InternalClient.java
@@ -360,6 +360,10 @@ public class InternalClient
      *	      the service checking for availability of new messages. The value
      *	      is expected to be of type {@code long}.
      *
+     *      - <b>SetSendInterval</b> - this option is applicable to all protocols.
+     *	      This value sets the period (in milliseconds) that this SDK spawns a thread to send a queued message.
+     *	      Even if no message is queued, this thread will be spawned.
+     *
      *	    - <b>SetReceiveInterval</b> - this option is applicable to all protocols
      *	      in case of HTTPS protocol, this option acts the same as {@code SetMinimumPollingInterval}
      *	      in case of MQTT and AMQP protocols, this option specifies the interval in millisecods
