bcf664a08152f46ef240e8a56061a7a2375756f6
timtay-microsoft <timtay@microsoft.com>
2020-06-18 12:35:07 -0700

    set default connect timeout to 24 seconds, too

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/RegistryManagerOptions.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/RegistryManagerOptions.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public class RegistryManagerOptions
 {
     protected static final Integer DEFAULT_HTTP_READ_TIMEOUT_MS = 24000; // 24 seconds
-    protected static final Integer DEFAULT_HTTP_CONNECT_TIMEOUT_MS = 0; // infinite
+    protected static final Integer DEFAULT_HTTP_CONNECT_TIMEOUT_MS = 24000; // 24 seconds
 
     /**
      * The options that specify what proxy to tunnel through. If null, no proxy will be used
