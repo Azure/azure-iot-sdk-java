d5bb8706729bfa589d1dceaecafeb9d8afb0f13f
bikamani <41314966+bikamani@users.noreply.github.com>
2020-06-18 15:29:13 -0700

    intendation in yaml and ignore dt e2e tests

--- digital-twin/e2e-tests/digital-twin-e2e-tests-common/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/e2e/tests/DigitalTwinE2ETests.java
+++ digital-twin/e2e-tests/digital-twin-e2e-tests-common/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/e2e/tests/DigitalTwinE2ETests.java
@@ -3,10 +3,12 @@ package com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
+import org.junit.Ignore;
 
 import static com.microsoft.azure.sdk.iot.digitaltwin.e2e.helpers.Tools.retrieveEnvironmentVariableValue;
 import static org.junit.Assume.assumeTrue;
 
+@Ignore
 public class DigitalTwinE2ETests {
     private static boolean runDigitalTwinTests = Boolean.parseBoolean(retrieveEnvironmentVariableValue("RUN_DIGITAL_TWIN_TESTS", "true"));
     private static boolean isBasicTier = Boolean.parseBoolean(retrieveEnvironmentVariableValue("IS_BASIC_TIER_HUB"));
