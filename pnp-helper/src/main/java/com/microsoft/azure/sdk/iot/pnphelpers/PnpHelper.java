f5027d4490d7b6ed9c82586ddbb0500425e69056
vinagesh <vinagesh@outlook.com>
2020-07-08 16:21:27 -0700

    feat(samples): Updating and adding samples for pnp summer refresh (#841)

--- pnp-helper/src/main/java/com/microsoft/azure/sdk/iot/pnphelpers/PnpHelper.java
+++ pnp-helper/src/main/java/com/microsoft/azure/sdk/iot/pnphelpers/PnpHelper.java
@@ -9,6 +9,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.microsoft.azure.sdk.iot.device.DeviceTwin.Property;
 import com.microsoft.azure.sdk.iot.device.Message;
+import com.microsoft.azure.sdk.iot.service.devicetwin.Pair;
 import lombok.NonNull;
 
 import java.nio.charset.StandardCharsets;
@@ -209,4 +210,39 @@ public class PnpHelper {
         return jsonObject.get("commandRequest").getAsJsonObject().get("value");
     }
 
+    /**
+     * Create a key-value property patch for twin update. This patch is to update a property on a component of a device.
+     * @param propertyName The property name, as defined in the DTDL interface.
+     * @param propertyValue The property value, in the format defined in the DTDL interface.
+     * @param componentName The name of the component on which to update the property.
+     * @return The property patch for twin update.
+     *
+     * The property patch should be in the below format:
+     *   "componentName": {
+     *      "__t": "c",
+     *      "propertyName": {
+     *        "value": "hello"
+     *      }
+     */
+    public static Set<Pair> CreateComponentPropertyPatch(@NonNull String propertyName, @NonNull double propertyValue, @NonNull String componentName)
+    {
+        JsonObject patchJson = new JsonObject();
+        patchJson.addProperty(PROPERTY_COMPONENT_IDENTIFIER_KEY, PROPERTY_COMPONENT_IDENTIFIER_VALUE);
+        patchJson.addProperty(propertyName, propertyValue);
+        return singleton(new Pair(componentName, patchJson));
+    }
+
+    /**
+     * Helper to construct the command to call on a component of a device..
+     * @param componentName The name of the component on which to invoke the command.
+     * @param commandName The name of the command to invoke.
+     * @return The command name to invoke.
+     *
+     * The command to invoke for components should be in the format:
+     * "componentName*commandName"
+     */
+    public static String CreateComponentCommandName(String componentName, String commandName)
+    {
+        return componentName + "*" + commandName;
+    }
 }
