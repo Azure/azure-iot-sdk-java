2d5a217421ffb5669584f99d71a3d2891ddf5e07
vinagesh <vinagesh@outlook.com>
2020-07-08 16:21:27 -0700

    feat(samples): Updating and adding samples for pnp summer refresh (#841)

--- device/iot-device-samples/pnp-device-sample/TemperatureController/src/main/java/samples/com/microsoft/azure/sdk/iot/device/TemperatureController.java
+++ device/iot-device-samples/pnp-device-sample/TemperatureController/src/main/java/samples/com/microsoft/azure/sdk/iot/device/TemperatureController.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-package samples.com.microsoft.azure.sdk.iot;
+package samples.com.microsoft.azure.sdk.iot.device;
 
 import com.microsoft.azure.sdk.iot.deps.twin.TwinCollection;
 import com.microsoft.azure.sdk.iot.device.*;
