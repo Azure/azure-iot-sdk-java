cec1b030b0ecb876658e6e46f677844364598f14
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-08-13 10:58:34 -0700

    refactor(iot-device): Add comments for each connection status change state and reason returned

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/IotHubConnectionStatusChangeReason.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/IotHubConnectionStatusChangeReason.java
@@ -7,24 +7,52 @@ package com.microsoft.azure.sdk.iot.device;
 
 public enum IotHubConnectionStatusChangeReason
 {
-    // The SAS token used to open the connection has expired
+    /**
+     *  The SAS token associated with the client has expired, and cannot be renewed.
+     *  The supplied credentials need to be fixed before a connection can be established.
+     *  <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED}.</p>
+     */
     EXPIRED_SAS_TOKEN,
 
-    // The SAS token/Certificates used to open the connection are rejected by the service with UNAUTHORIZED
+    /**
+     * Incorrect credentials were supplied to the client instance.
+     * The supplied credentials need to be fixed before a connection can be established.
+     * <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED}.</p>
+     */
     BAD_CREDENTIAL,
 
-    // The SDK has exhausted its retry policy and has stopped retrying
+    /**
+     * The client was disconnected due to a transient exception, but the retry policy expired before a connection could be re-established.
+     * If you want to perform more operations on the device client, you should {@link DeviceClient#closeNow()} and then {@link DeviceClient#open()} the client.
+     * <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED}.</p>
+     */
     RETRY_EXPIRED,
 
-    // The SDK encountered a retryable exception (IOExceptions, etc.)
+    /**
+     * The client was disconnected due to loss of network, the client will attempt for recovery.
+     * <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED_RETRYING}.</p>
+     */
     NO_NETWORK,
 
-    // The default case, only given when no other change reason is applicable. See the thrown exception for more details
+    /**
+     * This can be returned with either a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED} or {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED_RETRYING}.
+     * <p>When returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED_RETRYING}, this signifies that the client is trying to recover from a disconnect due to a transient exception.
+     * Do NOT close or open the client instance. Once the client successfully reports {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#CONNECTED}, operations will be resumed.</p>
+     * <p>When returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED}, this signifies that client is disconnected due to a non-retryable exception.
+     * You should inspect the throwable supplied in the {@link IotHubConnectionStatusChangeCallback} to determine what action needs to be taken.
+     * If you want to perform more operations on the device client, you should {@link DeviceClient#closeNow()} and then {@link DeviceClient#open()} the client.</p>
+     */
     COMMUNICATION_ERROR,
 
-    // The SDK opened the connection successfully
+    /**
+     * The client is connected, and ready to be used.
+     * <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#CONNECTED}.</p>
+     */
     CONNECTION_OK,
 
-    // The SDK closed the connection because the user closed the client that was using it
+    /**
+     * The client has been closed gracefully.
+     *  <p>This is returned with a connection status of {@link com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus#DISCONNECTED}.</p>
+     */
     CLIENT_CLOSE
 }
--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/IotHubConnectionStatus.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/IotHubConnectionStatus.java
@@ -5,7 +5,21 @@ package com.microsoft.azure.sdk.iot.device.transport;
 
 public enum IotHubConnectionStatus
 {
+    /**
+     * The device or module is connected.
+     * <p>The client is connected, and ready to be used.</p>
+     */
     CONNECTED,
+
+    /**
+     * The device or module is disconnected.
+     * <p>Inspect the associated {@link com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeReason} returned (and exception thrown, if any), and take appropriate action.</p>
+     */
     DISCONNECTED,
+
+    /**
+     * The device or module is attempting to reconnect.
+     * <p>The client is attempting to recover the connection. Do NOT close or open the client instance when it is retrying.</p>
+     */
     DISCONNECTED_RETRYING
 }
