8c1efddce21b957b849afdea6e43f7199639fa75
timtay-microsoft <timtay@microsoft.com>
2020-08-26 12:07:01 -0700

    feat(prov-serv): Add APIs for getAttestationMechanism for individual and group enrollments
    These new APIs allow you to retrieve the symmetric keys for enrollments using symmetric key attestation

---
+++ iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/provisioning/ProvisioningServiceClientAndroidRunner.java
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package com.microsoft.azure.sdk.iot.android.provisioning;
+
+import com.microsoft.azure.sdk.iot.android.helper.TestGroup1;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+
+import tests.integration.com.microsoft.azure.sdk.iot.provisioning.ProvisioningServiceClientTests;
+
+@TestGroup1
+public class ProvisioningServiceClientAndroidRunner extends ProvisioningServiceClientTests
+{
+
+}
