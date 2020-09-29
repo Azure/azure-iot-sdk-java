7c3467b9a3b8ea461c2776ad39b50e91d186c7ab
bikamani <41314966+bikamani@users.noreply.github.com>
2020-09-28 21:13:34 -0700

    delombok

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/digitaltwin/models/WritableProperty.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/digitaltwin/models/WritableProperty.java
@@ -4,8 +4,6 @@
 package com.microsoft.azure.sdk.iot.service.digitaltwin.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
@@ -18,48 +16,83 @@ public final class WritableProperty
      * The desired value of a property.
      */
     @JsonProperty("desiredValue")
-    @Getter
-    @Setter
     public Object DesiredValue;
 
     /**
      * The version of the property with the specified desired value.
      */
     @JsonProperty("desiredVersion")
-    @Getter
-    @Setter
     public int DesiredVersion;
 
     /**
      * The version of the reported property value
      */
     @JsonProperty("ackVersion")
-    @Getter
-    @Setter
     public int AckVersion;
 
     /**
      * The response code of the property update request, usually an HTTP Status Code (e.g. 200).
      */
     @JsonProperty("ackCode")
-    @Getter
-    @Setter
     public int AckCode;
 
     /**
      * The message response of the property update request.
      */
     @JsonProperty("ackDescription")
-    @Getter
-    @Setter
     public String AckDescription;
 
     /**
      * The time when this property was last updated.
      */
     @JsonProperty("ackDescription")
-    @Getter
-    @Setter
     public OffsetDateTime LastUpdateTime;
 
+    public Object getDesiredValue() {
+        return this.DesiredValue;
+    }
+
+    public int getDesiredVersion() {
+        return this.DesiredVersion;
+    }
+
+    public int getAckVersion() {
+        return this.AckVersion;
+    }
+
+    public int getAckCode() {
+        return this.AckCode;
+    }
+
+    public String getAckDescription() {
+        return this.AckDescription;
+    }
+
+    public OffsetDateTime getLastUpdateTime() {
+        return this.LastUpdateTime;
+    }
+
+    public void setDesiredValue(Object DesiredValue) {
+        this.DesiredValue = DesiredValue;
+    }
+
+    public void setDesiredVersion(int DesiredVersion) {
+        this.DesiredVersion = DesiredVersion;
+    }
+
+    public void setAckVersion(int AckVersion) {
+        this.AckVersion = AckVersion;
+    }
+
+    public void setAckCode(int AckCode) {
+        this.AckCode = AckCode;
+    }
+
+    public void setAckDescription(String AckDescription) {
+        this.AckDescription = AckDescription;
+    }
+
+    public void setLastUpdateTime(OffsetDateTime LastUpdateTime) {
+        this.LastUpdateTime = LastUpdateTime;
+    }
 }
