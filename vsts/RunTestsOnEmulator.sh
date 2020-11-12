9fb619a634ae5bde0f6a999672f5aa6353c88c71
timtay-microsoft <timtay@microsoft.com>
2020-11-12 10:03:48 -0800

    Revert "add verbose logging to android tests"
    This reverts commit 8a9046cb7f54fb2b576240a9e55da4ab21ffb040.

--- vsts/RunTestsOnEmulator.sh
+++ vsts/RunTestsOnEmulator.sh
@@ -14,8 +14,6 @@ annotationString="com.microsoft.azure.sdk.iot.android.helper.${TEST_GROUP_ID}"
 echo 'Running android tests with annotation'
 echo $annotationString
 
-adb logcat *:V
-
 #Return code from adb shell isn't returned as one would expect. Need to capture output logs and analyze them to determine if this test run was a success or not
 TestLogs=$(adb -e shell am instrument -w -e annotation $annotationString com.iothub.azure.microsoft.com.androide2e.test/android.support.test.runner.AndroidJUnitRunner)
 
