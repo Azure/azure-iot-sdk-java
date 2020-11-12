a3f1dd27c19dc4f3cbd003fc0d7fb71c6b7b54b6
timtay-microsoft <timtay@microsoft.com>
2020-11-12 15:32:47 -0800

    fixup

--- iot-e2e-tests/common/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/helpers/Tools.java
+++ iot-e2e-tests/common/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/helpers/Tools.java
@@ -173,7 +173,7 @@ public class Tools
                 {
                     try
                     {
-                        log.debug("Registering a batch of {} devices from total of {} devices", subIterable.size(), devices.size());
+                        log.debug("Registering a batch of {} devices from total of {} remaining devices", subIterable.size(), devicesClone.size() + subIterable.size());
                         addDevices(subIterable, connectionString);
                         break;
                     }
