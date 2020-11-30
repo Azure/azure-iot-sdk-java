333c915668a256c0de12d7b89cb5acb126a623f3
Azad Abbasi <azabbasi@microsoft.com>
2020-11-30 22:49:29 +0000

    Multiplexing samples: Device Client Manager and Multiplexing client manager (#993)

--- device/iot-device-samples/multiplexing-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/MultiplexingSample.java
+++ device/iot-device-samples/multiplexing-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/MultiplexingSample.java
@@ -4,96 +4,35 @@
 package samples.com.microsoft.azure.sdk.iot;
 
 import com.microsoft.azure.sdk.iot.device.*;
-import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Sample that demonstrates creating a multiplexed connection to IoT Hub using AMQPS / AMQPS_WS. It also demonstrates
  * removing and adding device clients from the multiplexed connection while it is open.
+ *
+ * This sample also demonstrates how reconnection can be handled by introducing {@link MultiplexClientManager} and {@link DeviceClientManager}
+ * both of which extend {@link ClientManagerBase}.
+ *
+ * ClientManagerBase class implements two separate interfaces
+ *
+ * 1. {@link IotHubConnectionStatusChangeCallback} which is an SDK type for handling a connection status change callback
+ * 2. {@link ConnectionStatusTracker} which allows for a dependent connection check the underlying connection status before attempting a reconnection.
+ *
+ * Both {@link DeviceClientManager} and {@link MultiplexClientManager} classes delegate all but 3 API calls to their underlying SDK client (MultiplexingClient or DeviceClient) they are wrapping around.
+ * open(), close(), registerConnectionStatusChangeCallback() are the 3 APIs that are handled by the ClientManager instance to allow for dynamic reconnection logic.
+ *
+ * These client managers are in charge of handling the reconnection logic since they are the connection status callback handler.
+ *
+ * DeviceClientManager also takes an instance of {@link ConnectionStatusTracker} to identify whether or not the underlying
+ * Multiplexing connection is established or not before they attempt a re-connection in an event of a disconnection.
+ * This will avoid unnecessary reconnection attempts by DeviceClient since it cannot be connected unless the Multiplexing connection is established.
+ *
  */
 public class MultiplexingSample
 {
-    // Every multiplexed device will maintain its own connection status callback. Because of that, you can monitor
-    // if a particular device session goes offline unexpectedly.
-    public static class MultiplexedDeviceConnectionStatusChangeTracker implements IotHubConnectionStatusChangeCallback
-    {
-        private boolean isOpen = false;
-        private boolean isDisconnectedRetrying = false;
-
-        public boolean isOpen() {
-            return isOpen;
-        }
-
-        public boolean isDisconnectedRetrying() {
-            return isDisconnectedRetrying;
-        }
-
-        @Override
-        public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason statusChangeReason, Throwable throwable, Object callbackContext)
-        {
-            // In this sample, both the device clients and the multiplexing client use this class to track their respective connection statuses.
-            // If the context is not null, it has the device id that the status belongs to. If the context is null, the status
-            // update belongs to the multiplexed connection itself.
-            String deviceId = (String) callbackContext;
-
-            if (throwable == null)
-            {
-                if (deviceId == null)
-                {
-                    System.out.println("CONNECTION STATUS UPDATE FOR MULTIPLEXED CONNECTION - " + status + ", " + statusChangeReason);
-                }
-                else
-                {
-                    System.out.println("CONNECTION STATUS UPDATE FOR DEVICE " + deviceId + " - " + status + ", " + statusChangeReason);
-                }
-            }
-            else
-            {
-                if (deviceId == null)
-                {
-                    System.out.println("CONNECTION STATUS UPDATE FOR MULTIPLEXED CONNECTION - " + status + ", " + statusChangeReason + ", " + throwable.getMessage());
-                }
-                else
-                {
-                    System.out.println("CONNECTION STATUS UPDATE FOR DEVICE " + deviceId + " - " + status + ", " + statusChangeReason + ", " + throwable.getMessage());
-                }
-            }
-
-            if (status == IotHubConnectionStatus.CONNECTED)
-            {
-                isOpen = true;
-                isDisconnectedRetrying = false;
-            }
-            else if (status == IotHubConnectionStatus.DISCONNECTED)
-            {
-                isOpen = false;
-                isDisconnectedRetrying = false;
-            }
-            else if (status == IotHubConnectionStatus.DISCONNECTED_RETRYING)
-            {
-                isDisconnectedRetrying = true;
-                isOpen = false;
-            }
-        }
-    }
-
-    public static int acknowledgedSentMessages = 0;
-    protected static class TelemetryAcknowledgedEventCallback implements IotHubEventCallback
-    {
-        public void execute(IotHubStatusCode status, Object context)
-        {
-            String messageId = (String) context;
-            System.out.println("IoT Hub responded to message "+ messageId  + " with status " + status.name());
-            acknowledgedSentMessages++;
-        }
-    }
-
     /**
      * Multiplex devices an IoT Hub using AMQPS / AMQPS_WS
      *
@@ -102,6 +41,7 @@ public class MultiplexingSample
      * args[1] = host name to connect to (for instance, "my-iot-hub.azure-devices.net")
      * args[2] = IoT Hub connection string - Device Client 1
      * args[3] = IoT Hub connection string - Device Client 2
+     * Add up to 998 device connection strings from args[4] on.
      *
      * Any additional arguments will be interpreted as additional connections strings. This allows this sample to be
      * run with more than 2 devices. At
@@ -119,7 +59,10 @@ public class MultiplexingSample
                             + "1. [Protocol]                   - amqps | amqps_ws\n"
                             + "2. [HostName]                   - my-iot-hub.azure-devices.net\n"
                             + "3. [Device 1 connection string] - String containing Hostname, Device Id & Device Key in one of the following formats: HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>\n"
-                            + "4. [Device 2 connection string] - String containing Hostname, Device Id & Device Key in one of the following formats: HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>\n",
+                            + "4. [Device 2 connection string] - String containing Hostname, Device Id & Device Key in one of the following formats: HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>\n"
+                            + ".\n"
+                            + ".\n"
+                            + ".\n",
                     args.length);
             return;
         }
@@ -132,33 +75,34 @@ public class MultiplexingSample
 
         String hostName = args[1];
 
-        // This sample allows for users to pass in 2 to many device connection strings, so this sample may multiplex
-        // 2 to many devices
-        int multiplexedDeviceCount = args.length - 2;
+        // This sample requires users for pass in at least 2 device connection strings, so this sample may multiplex
+        final int multiplexedDeviceCount = args.length - 2;
 
         // Options include setting a custom SSLContext to use for the SSL handshake, configuring proxy support, and setting threading strategies
         MultiplexingClientOptions options = MultiplexingClientOptions.builder().build();
-        MultiplexingClient multiplexingClient = new MultiplexingClient(hostName, protocol, options);
-        MultiplexedDeviceConnectionStatusChangeTracker multiplexedConnectionStatusTracker = new MultiplexedDeviceConnectionStatusChangeTracker();
-        multiplexingClient.registerConnectionStatusChangeCallback(multiplexedConnectionStatusTracker, null);
+        final MultiplexingClient multiplexingClient = new MultiplexingClient(hostName, protocol, options);
+
+        MultiplexClientManager multiplexClientManager = new MultiplexClientManager(multiplexingClient, "MultiplexingClient");
 
         System.out.println("Creating device clients that will be multiplexed");
-        List<String> deviceIds = new ArrayList<>();
-        Map<String, DeviceClient> multiplexedDeviceClients = new HashMap<>(multiplexedDeviceCount);
-        List<MultiplexedDeviceConnectionStatusChangeTracker> deviceConnectionStatusTrackers = new ArrayList<>(multiplexedDeviceCount);
+        final List<String> deviceIds = new ArrayList<>();
+        final Map<String, DeviceClient> multiplexedDeviceClients = new HashMap<>(multiplexedDeviceCount);
+        final Map<String, DeviceClientManager> deviceManagers = new HashMap<>(multiplexedDeviceCount);
+
         for (int i = 0; i < multiplexedDeviceCount; i++)
         {
             DeviceClient clientToMultiplex = new DeviceClient(args[i+2], protocol);
             String deviceId = clientToMultiplex.getConfig().getDeviceId();
             deviceIds.add(deviceId);
+
             multiplexedDeviceClients.put(deviceId, clientToMultiplex);
-            deviceConnectionStatusTrackers.add(new MultiplexedDeviceConnectionStatusChangeTracker());
-            multiplexedDeviceClients.get(deviceId).registerConnectionStatusChangeCallback(deviceConnectionStatusTrackers.get(i), deviceId);
+
+            deviceManagers.put(deviceId, new DeviceClientManager(clientToMultiplex, multiplexClientManager));
         }
 
         System.out.println("Opening multiplexed connection");
         // All previously registered device clients will be opened alongside this multiplexing client
-        multiplexingClient.open();
+        multiplexClientManager.open();
         System.out.println("Multiplexed connection opened successfully");
 
         // Note that all the clients are registered at once. This method will asynchronously start the registration
@@ -166,8 +110,10 @@ public class MultiplexingSample
         // If instead each client was registered separately through multiplexingClient.registerDeviceClient(), it would
         // take a longer time since it would block on each registration completing, rather than block on all registrations completing
         System.out.println("Registering " + multiplexedDeviceCount + " clients to the multiplexing client...");
-        multiplexingClient.registerDeviceClients(multiplexedDeviceClients.values());
+        multiplexClientManager.registerDeviceClients(multiplexedDeviceClients.values());
+
         System.out.println("Successfully registered " + multiplexedDeviceCount + " clients to the multiplexing client");
+
 
         for (String deviceId : deviceIds)
         {
@@ -193,22 +139,34 @@ public class MultiplexingSample
         String deviceIdToUnregister = deviceIds.get(0);
 
         System.out.println("Unregistering device " + deviceIdToUnregister + " from multiplexed connection...");
-        multiplexingClient.unregisterDeviceClient(multiplexedDeviceClients.get(deviceIdToUnregister));
+        multiplexClientManager.unregisterDeviceClient(multiplexedDeviceClients.get(deviceIdToUnregister));
         System.out.println("Successfully unregistered device " + deviceIdToUnregister + " from an active multiplexed connection.");
 
         // This code demonstrates how to add a device to an active multiplexed connection without shutting down
         // the whole multiplexed connection or any of the other devices.
         System.out.println("Re-registering device " + deviceIdToUnregister + " to an active multiplexed connection...");
-        multiplexingClient.registerDeviceClient(multiplexedDeviceClients.get(deviceIdToUnregister));
+        multiplexClientManager.registerDeviceClient(multiplexedDeviceClients.get(deviceIdToUnregister));
         System.out.println("Successfully registered " + deviceIdToUnregister + " to an active multiplexed connection");
 
         // Before closing a multiplexing client, you do not need to unregister all of the registered clients.
         // If they are not unregistered, then you can re-open the multiplexing client later and it will still
         // have all of your registered devices
-
         System.out.println("Closing entire multiplexed connection...");
         // This call will close all multiplexed device client instances as well
-        multiplexingClient.close();
+        multiplexClientManager.closeClient();
         System.out.println("Successfully closed the multiplexed connection");
+
+        System.out.println("Shutting down...");
+    }
+
+    private static int acknowledgedSentMessages = 0;
+    private static class TelemetryAcknowledgedEventCallback implements IotHubEventCallback
+    {
+        public void execute(IotHubStatusCode status, Object context)
+        {
+            String messageId = (String) context;
+            System.out.println("IoT Hub responded to message "+ messageId  + " with status " + status.name());
+            acknowledgedSentMessages++;
+        }
     }
 }
