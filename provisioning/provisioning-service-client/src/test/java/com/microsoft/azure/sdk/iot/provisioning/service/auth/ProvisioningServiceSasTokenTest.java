8dd6b836a5a053e7f1f2df515e48728410cbf3a3
timtay-microsoft <timtay@microsoft.com>
2021-08-12 13:44:11 -0700

    Rename unit test packages to match the packages of the classes they test (#1308)
    This is necessary if we want to make a package private class in our shipped code still be unit testable
    
    In the upcoming 2.0.0 version we will mark a lot of public classes as package private in order to hide them from users. This change isn't a breaking change though, so it can go directly into our master branch

---
+++ provisioning/provisioning-service-client/src/test/java/com/microsoft/azure/sdk/iot/provisioning/service/auth/ProvisioningServiceSasTokenTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package com.microsoft.azure.sdk.iot.provisioning.service.auth;
+
+import com.microsoft.azure.sdk.iot.provisioning.service.auth.ProvisioningConnectionString;
+import com.microsoft.azure.sdk.iot.provisioning.service.auth.ProvisioningConnectionStringBuilder;
+import com.microsoft.azure.sdk.iot.provisioning.service.auth.ProvisioningSasToken;
+import mockit.Deencapsulation;
+import mockit.Expectations;
+import org.junit.Test;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import static org.apache.commons.codec.binary.Base64.decodeBase64;
+import static org.apache.commons.codec.binary.Base64.encodeBase64String;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for Provisioning Service SasToken
+ * 100% methods, 100% lines covered
+ */
+public class ProvisioningServiceSasTokenTest
+{
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_001: [The constructor shall throw IllegalArgumentException if the input object is null]
+    @Test (expected = IllegalArgumentException.class)
+    public void constructorThrowsOnNullConnectionString() throws IllegalArgumentException
+    {
+        // Arrange
+        ProvisioningConnectionString provisioningConnectionString = null;
+        // Act
+        ProvisioningSasToken provisioningServiceSasToken = new ProvisioningSasToken(provisioningConnectionString);
+    }
+
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_002: [The constructor shall create a target uri from the url encoded host name)]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_003: [The constructor shall create a string to sign by concatenating the target uri and the expiry time string]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_004: [The constructor shall create a key from the shared access key signing with HmacSHA256]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_005: [The constructor shall compute the final signature by url encoding the signed key]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_006: [The constructor shall concatenate the target uri, the signature, the expiry time and the key name using the format: "SharedAccessSignature sr=%s&sig=%s&se=%s&skn=%s"]
+    @Test
+    public void constructorSucceeded() throws Exception
+    {
+        // Arrange
+        String cryptoProvider = "HmacSHA256";
+        String charset = "UTF-8";
+        String deviceProvisioningServiceName = "b.c.d";
+        String hostName = "HOSTNAME." + deviceProvisioningServiceName;
+        String sharedAccessKeyName = "ACCESSKEYNAME";
+        String policyName = "SharedAccessKey";
+        String sharedAccessKey = encodeBase64String("key".getBytes());
+        String connectionString = "HostName=" + hostName + ";SharedAccessKeyName=" + sharedAccessKeyName + ";" + policyName + "=" + sharedAccessKey;
+
+        ProvisioningConnectionString provisioningConnectionString = ProvisioningConnectionStringBuilder.createConnectionString(connectionString);
+
+        // Assert
+        new Expectations()
+        {
+            URLEncoder urlEncoder;
+            System system;
+            final SecretKeySpec secretKeySpec;
+            Mac mac;
+            {
+                URLEncoder.encode(hostName.toLowerCase(),String.valueOf(StandardCharsets.UTF_8));
+                System.currentTimeMillis();
+                decodeBase64(sharedAccessKey.getBytes(charset));
+                byte[] body = { 1 };
+                secretKeySpec = new SecretKeySpec(body, cryptoProvider);
+                Mac.getInstance(cryptoProvider);
+            }
+        };
+        // Act
+        ProvisioningSasToken provisioningServiceSasToken = new ProvisioningSasToken(provisioningConnectionString);
+    }
+
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_002: [The constructor shall create a target uri from the url encoded host name)]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_003: [The constructor shall create a string to sign by concatenating the target uri and the expiry time string (one year)]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_004: [The constructor shall create a key from the shared access key signing with HmacSHA256]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_005: [The constructor shall compute the final signature by url encoding the signed key]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_006: [The constructor shall concatenate the target uri, the signature, the expiry time and the key name using the format: "SharedAccessSignature sr=%s&sig=%s&se=%s&skn=%s"]
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_008: [The function shall return with the generated token]
+    @Test
+    public void constructorCheckFormatSucceeded() throws Exception
+    {
+        // Arrange
+        String deviceProvisioningServiceName = "b.c.d";
+        String hostName = "HOSTNAME." + deviceProvisioningServiceName;
+        String sharedAccessKeyName = "ACCESSKEYNAME";
+        String policyName = "SharedAccessKey";
+        String sharedAccessKey = encodeBase64String("key".getBytes());
+        String connectionString = "HostName=" + hostName + ";SharedAccessKeyName=" + sharedAccessKeyName + ";" + policyName + "=" + sharedAccessKey;
+        ProvisioningConnectionString provisioningConnectionString = ProvisioningConnectionStringBuilder.createConnectionString(connectionString);
+
+        // Act
+        ProvisioningSasToken provisioningServiceSasToken = new ProvisioningSasToken(provisioningConnectionString);
+        String token = provisioningServiceSasToken.toString();
+
+        // Assert
+        assertTrue(token.contains("SharedAccessSignature sr=hostname.b.c.d&sig="));
+        assertTrue(token.contains("&se="));
+        assertTrue(token.contains("&skn=ACCESSKEYNAME"));
+    }
+
+    // Tests_SRS_PROVISIONING_SERVICE_SASTOKEN_12_007: [The constructor shall throw Exception if building the token failed]
+    @Test (expected = Exception.class)
+    public void constructorThrowsOnBuildToken() throws Exception
+    {
+        // Arrange
+        String deviceProvisioningServiceName = "b.c.d";
+        String hostName = "HOSTNAME." + deviceProvisioningServiceName;
+        String sharedAccessKeyName = "ACCESSKEYNAME";
+        String policyName = "SharedAccessKey";
+        String sharedAccessKey = encodeBase64String("key".getBytes());
+        String connectionString = "HostName=" + hostName + ";SharedAccessKeyName=" + sharedAccessKeyName + ";" + policyName + "=" + sharedAccessKey;
+
+        // Act
+        ProvisioningConnectionString provisioningConnectionString = ProvisioningConnectionStringBuilder.createConnectionString(connectionString);
+        Deencapsulation.setField(provisioningConnectionString, "hostName", null);
+        ProvisioningSasToken provisioningServiceSasToken = new ProvisioningSasToken(provisioningConnectionString);
+    }
+}
