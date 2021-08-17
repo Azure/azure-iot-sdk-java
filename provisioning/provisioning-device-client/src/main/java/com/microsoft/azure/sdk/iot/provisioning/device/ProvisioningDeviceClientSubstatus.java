fd76f2b6999104e0322564ccd770c5fcfba0f26b
timtay-microsoft <timtay@microsoft.com>
2021-08-17 11:18:33 -0700

    feat(dps-dev): Add missing registration result fields
    The service has sent these fields for a while, but we never exposed them to users for some reason. This brings our registration result class up to par with what the service sends to the SDK

---
+++ provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/ProvisioningDeviceClientSubstatus.java
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.azure.sdk.iot.provisioning.device;
+
+/**
+ * The possible substatus values of a successful device provisioning.
+ */
+public enum ProvisioningDeviceClientSubstatus
+{
+    /**
+     * Device has been assigned to an IoT hub for the first time
+     */
+    INITIAL_ASSIGNMENT("initialAssignment"),
+
+    /**
+     * Device has been assigned to a different IoT hub and its device data was migrated from the previously assigned
+     * IoT hub. Device data was removed from the previously assigned IoT hub.
+     */
+    DEVICE_DATA_MIGRATED("deviceDataMigrated"),
+
+    /**
+     * Device has been assigned to a different IoT hub and its device data was populated from the initial state stored
+     * in the enrollment. Device data was removed from the previously assigned IoT hub.
+     */
+    DEVICE_DATA_RESET("deviceDataReset"),
+
+    /**
+     * Device has been re-provisioned to a previously assigned IoT hub.
+     */
+    REPROVISIONED_TO_INITIAL_ASSIGNMENT("reprovisionedToInitialAssignment");
+
+    private final String substatus;
+
+    ProvisioningDeviceClientSubstatus(String substatus)
+    {
+        this.substatus = substatus;
+    }
+
+    public String getValue()
+    {
+        return this.substatus;
+    }
+
+    public static ProvisioningDeviceClientSubstatus fromString(String substatus)
+    {
+        for (ProvisioningDeviceClientSubstatus substatusCode : ProvisioningDeviceClientSubstatus.values())
+        {
+            if (substatusCode.substatus.equalsIgnoreCase(substatus))
+            {
+                return substatusCode;
+            }
+        }
+
+        return null;
+    }
+}
