8bda812ba0c6d3558a8545cfc45f615e12be4208
James Davis <jamdavi@microsoft.com>
2021-09-02 14:28:09 -0600

    Remove header

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/TelemetryMessage.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/TelemetryMessage.java
@@ -1,21 +1,31 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package com.microsoft.azure.sdk.iot.device.convention;
 
+import com.microsoft.azure.sdk.iot.deps.convention.PayloadConvention;
 import com.microsoft.azure.sdk.iot.device.Message;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The extension of Message that uses a {@link PayloadConvention} to generate the message payload.
+ */
 public class TelemetryMessage extends Message
 {
+    /**
+     * Telemetry collection to be sent for this message
+     */
     @Getter
     @Setter
-    public ClientPropertyCollection telemetry;
+    PayloadCollection telemetry;
 
     @Override
     public byte[] getBytes()
     {
-        if (Telemetry.Convention != null && Telemetry.Convention.getPayloadSerializer() != null)
+        if (getTelemetry().getConvention() != null && getTelemetry().getConvention().getPayloadSerializer() != null)
         {
-            return Telemetry.Convention.GetObjectBytes(Telemetry);
+            return getTelemetry().getConvention().getObjectBytes(getTelemetry());
         }
         return super.getBytes();
     }
