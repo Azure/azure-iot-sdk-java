d7ac58fbeb87bac849982b2c86ea1eac3ec877b5
James Davis <jamdavi@microsoft.com>
2021-09-02 14:24:11 -0600

    Addressing feedback

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/ClientPropertiesCallback.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/ClientPropertiesCallback.java
@@ -1,6 +1,17 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package com.microsoft.azure.sdk.iot.device.convention;
 
+/**
+ * The interface for retrieving client properties from a convention based device.
+ */
 public interface ClientPropertiesCallback
 {
+    /**
+     * The method to execute for the callback.
+     * @param responseStatus The response status of the client.
+     * @param callbackContext User supplied context for this callback. Can be {@code null} if unused.
+     */
     void execute(ClientProperties responseStatus, Object callbackContext);
 }
