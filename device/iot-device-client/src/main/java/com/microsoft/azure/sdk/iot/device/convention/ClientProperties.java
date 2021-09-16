e02c9badd0dcdf73660789b416e57479f4c9a928
James Davis <jamdavi@microsoft.com>
2021-09-16 09:59:31 -0600

    Bringing changes for client property over

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/ClientProperties.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/convention/ClientProperties.java
@@ -3,10 +3,15 @@
 
 package com.microsoft.azure.sdk.iot.device.convention;
 
+import com.microsoft.azure.sdk.iot.deps.convention.ClientPropertyCollection;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.Property;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Container for the properties that are writable and reported from the client
@@ -22,4 +27,19 @@ public class ClientProperties
     @Setter
     private ClientPropertyCollection reportedFromClient;
 
+    /**
+     * Gets the entries of the ClientPropertyCollection as a set of Properties to be sent to the IoT hub service.
+     *
+     * @param clientPropertyCollection The client properties collection to convert
+     * @return A set of properties
+     */
+     public static Set<Property> getCollectionAsSetOfProperty(ClientPropertyCollection clientPropertyCollection)
+    {
+        HashSet<Property> toReturn = new HashSet<>();
+        for (Map.Entry<String, Object> entry : clientPropertyCollection.entrySet())
+        {
+            toReturn.add(new Property(entry.getKey(), entry.getValue()));
+        }
+        return toReturn;
+    }
 }
