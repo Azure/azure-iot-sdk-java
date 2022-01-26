ad81051affc440b0204844c15a72dd9ab67dc54d
timtay-microsoft <timtay@microsoft.com>
2022-01-25 16:56:07 -0800

    simplify options to save validation for later

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/ClientOptions.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/ClientOptions.java
@@ -62,33 +62,7 @@ public final class ClientOptions
      */
     @Getter
     @Builder.Default
-    private int keepAliveInterval = DEFAULT_KEEP_ALIVE_INTERVAL_IN_SECONDS;
-
-    /**
-     * Sets the keep alive interval in seconds. This value defines the
-     * maximum time interval between messages sent or received. It enables the
-     * client to detect if the server is no longer available, without having to wait
-     * for the TCP/IP timeout. The client will ensure that at least one message
-     * travels across the network within each keep alive period. In the absence of a
-     * data-related message during the time period, the client sends a very small
-     * "ping" message, which the server will acknowledge. The default value is 230 seconds.
-     *
-     * <p>
-     * This value is only used in stateful connection oriented protocols such as AMQPS, AMQPS_WS, MQTT, and MQTT_WS. If
-     * the client is using HTTPS, then this value is ignored.
-     * </p>
-     *
-     * @param keepAliveInterval the number of seconds that the keep alive interval will be. Must be greater than 0.
-     */
-    public void setKeepAliveInterval(int keepAliveInterval)
-    {
-        if (keepAliveInterval <= 0)
-        {
-            throw new IllegalArgumentException("Keep alive interval must be greater than 0 seconds");
-        }
-
-        this.keepAliveInterval = keepAliveInterval;
-    }
+    private final int keepAliveInterval = DEFAULT_KEEP_ALIVE_INTERVAL_IN_SECONDS;
 
     /**
      * This option is only applicable for HTTPS.
--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/MultiplexingClientOptions.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/MultiplexingClientOptions.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 import javax.net.ssl.SSLContext;
 
 import static com.microsoft.azure.sdk.iot.device.DeviceClientConfig.DEFAULT_KEEP_ALIVE_INTERVAL_IN_SECONDS;
+import static com.microsoft.azure.sdk.iot.device.MultiplexingClient.*;
 
 /**
  * The optional settings for creating a {@link MultiplexingClient}. If set, these values will supersede any device client
@@ -22,31 +23,29 @@ public class MultiplexingClientOptions
      * The details of the proxy to connect to, if set. If not set, the multiplexing client will not connect through a proxy.
      */
     @Getter
-    @Setter
-    private ProxySettings proxySettings;
+    private final ProxySettings proxySettings;
 
     /**
      * The SSLContext to use during SSL handshake with the server. If not set, a default SSLContext will be generated for you.
      */
     @Getter
-    @Setter
-    private SSLContext sslContext;
+    private final SSLContext sslContext;
 
     /**
      * The period, in seconds, for how often a thread will spawn to handle queued outgoing messages. For instance, if this
      * is set to 10, then a thread will spawn every 10 seconds. If unset, this will default to {@link MultiplexingClient#DEFAULT_SEND_PERIOD_MILLIS}.
      */
     @Getter
-    @Setter
-    private long sendPeriod;
+    @Builder.Default
+    private final long sendPeriod = DEFAULT_SEND_PERIOD_MILLIS;
 
     /**
      * The period, in seconds, for how often a thread will spawn to handle queued incoming messages. For instance, if this
      * is set to 10, then a thread will spawn every 10 seconds. If unset, this will default to {@link MultiplexingClient#DEFAULT_RECEIVE_PERIOD_MILLIS}.
      */
     @Getter
-    @Setter
-    private long receivePeriod;
+    @Builder.Default
+    private final long receivePeriod = DEFAULT_RECEIVE_PERIOD_MILLIS;
 
     /**
      * This option specifies how many messages a given send thread should attempt to send before exiting.
@@ -54,8 +53,8 @@ public class MultiplexingClientOptions
      * batch size messages are sent. If unset, this will default to {@link MultiplexingClient#DEFAULT_MAX_MESSAGES_TO_SEND_PER_THREAD}.
      */
     @Getter
-    @Setter
-    private int maxMessagesSentPerSendThread;
+    @Builder.Default
+    private final int maxMessagesSentPerSendThread = DEFAULT_MAX_MESSAGES_TO_SEND_PER_THREAD;
 
     /**
      * Gets the keep alive interval in seconds. This value defines the
@@ -68,26 +67,5 @@ public class MultiplexingClientOptions
      */
     @Getter
     @Builder.Default
-    public int keepAliveInterval = DEFAULT_KEEP_ALIVE_INTERVAL_IN_SECONDS;
-
-    /**
-     * Sets the keep alive interval in seconds. This value defines the
-     * maximum time interval between messages sent or received. It enables the
-     * client to detect if the server is no longer available, without having to wait
-     * for the TCP/IP timeout. The client will ensure that at least one message
-     * travels across the network within each keep alive period. In the absence of a
-     * data-related message during the time period, the client sends a very small
-     * "ping" message, which the server will acknowledge. The default value is 230 seconds.
-     *
-     * @param keepAliveInterval the number of seconds that the keep alive interval will be. Must be greater than 0.
-     */
-    public void setKeepAliveInterval(int keepAliveInterval)
-    {
-        if (keepAliveInterval <= 0)
-        {
-            throw new IllegalArgumentException("Keep alive interval must be greater than 0 seconds");
-        }
-
-        this.keepAliveInterval = keepAliveInterval;
-    }
+    public final int keepAliveInterval = DEFAULT_KEEP_ALIVE_INTERVAL_IN_SECONDS;
 }
