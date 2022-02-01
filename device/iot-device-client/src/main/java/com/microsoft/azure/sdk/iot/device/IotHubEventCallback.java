a10a5d149f5ab10f91cace6b3f9b4b5a95a274ad
James Davis ⛺️🏔 <jamdavi@microsoft.com>
2022-02-01 15:27:23 -0700

    Conflict errors

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/IotHubEventCallback.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/IotHubEventCallback.java
@@ -19,6 +19,4 @@ public interface IotHubEventCallback
      * @param callbackContext a custom context given by the developer.
      */
     void execute(IotHubStatusCode responseStatus, Object callbackContext);
-
-    void clientPropThing(IotHubStatusCode responseStatus, Object callbackContext);
 }
