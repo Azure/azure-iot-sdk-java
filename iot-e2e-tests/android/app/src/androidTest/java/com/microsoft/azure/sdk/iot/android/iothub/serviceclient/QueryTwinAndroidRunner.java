d4b55ea4d071300c4641824091ab2a86b6869759
timtay-microsoft <timtay@microsoft.com>
2022-02-01 16:30:56 -0800

    fix android build

--- iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothub/serviceclient/QueryTwinAndroidRunner.java
+++ iot-e2e-tests/android/app/src/androidTest/java/com/microsoft/azure/sdk/iot/android/iothub/serviceclient/QueryTwinAndroidRunner.java
@@ -3,7 +3,7 @@
  *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
  */
 
-package com.microsoft.azure.sdk.iot.android.iothub.twin;
+package com.microsoft.azure.sdk.iot.android.iothub.serviceclient;
 
 import com.microsoft.azure.sdk.iot.android.helper.TestGroup14;
 import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
@@ -15,10 +15,10 @@ import org.junit.runners.Parameterized;
 import java.io.IOException;
 
 import tests.integration.com.microsoft.azure.sdk.iot.helpers.ClientType;
-import tests.integration.com.microsoft.azure.sdk.iot.iothub.serviceclient.QueryTwinTests;
+import tests.integration.com.microsoft.azure.sdk.iot.iothub.serviceclient.QueryClientTests;
 
 @TestGroup14
 @RunWith(Parameterized.class)
-public class QueryTwinAndroidRunner extends QueryTwinTests
+public class QueryTwinAndroidRunner extends QueryClientTests
 {
 }
