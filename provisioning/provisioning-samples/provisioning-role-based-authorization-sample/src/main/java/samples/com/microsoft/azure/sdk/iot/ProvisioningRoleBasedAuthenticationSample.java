351e6bf8fbdb5706d862f2ecbb3b7e3e94527f0d
dylanbulfinMS <95251881+dylanbulfinMS@users.noreply.github.com>
2022-02-04 16:04:47 -0500

    Add SAS argument config file

--- provisioning/provisioning-samples/provisioning-role-based-authorization-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/ProvisioningRoleBasedAuthenticationSample.java
+++ provisioning/provisioning-samples/provisioning-role-based-authorization-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/ProvisioningRoleBasedAuthenticationSample.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package samples.com.microsoft.azure.sdk.iot;
 
 import com.azure.core.credential.TokenCredential;
