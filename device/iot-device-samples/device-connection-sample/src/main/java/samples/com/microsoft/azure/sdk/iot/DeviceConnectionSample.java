53e87e786525e4ec02ceb69b8afad89081861158
timtay-microsoft <timtay@microsoft.com>
2022-03-29 10:08:19 -0700

    fixup

--- device/iot-device-samples/device-connection-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/DeviceConnectionSample.java
+++ device/iot-device-samples/device-connection-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/DeviceConnectionSample.java
@@ -2,14 +2,11 @@ package samples.com.microsoft.azure.sdk.iot;
 
 import com.microsoft.azure.sdk.iot.device.DeviceClient;
 import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
-import com.microsoft.azure.sdk.iot.service.exceptions.IotHubException;
-
-import java.io.IOException;
 
 public class DeviceConnectionSample
 {
     // This sample is only useful for MQTT, MQTT_WS, AMQPS, and AMQPS_WS,  since they are the only stateful connection protocols.
-    public static void main(String[] args) throws InterruptedException, IOException, IotHubException
+    public static void main(String[] args)
     {
         final String connectionString = "";
         final IotHubClientProtocol protocol = IotHubClientProtocol.AMQPS;
