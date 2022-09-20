c82b1a7f4645c94bdcdccd78164f7db6109960bf
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2022-09-21 00:56:57 +0200

    Revert "fix the authentcationType enums values"
    This reverts commit 1539d7dba883a3a910bcae02e0f881b90631c533.

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/auth/AuthenticationType.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/auth/AuthenticationType.java
@@ -11,36 +11,7 @@ package com.microsoft.azure.sdk.iot.service.auth;
  */
 public enum AuthenticationType
 {
-    SAS(0,"sas"),
-    SELF_SIGNED(1, "selfSigned"),
-    CERTIFICATE_AUTHORITY(2,"certificateAuthority");
-
-    private int key;
-    private String value;
-
-    AuthenticationType(int key, String value){
-        this.key = key;
-        this.value = value;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString(){
-        return this.value;
-    }
+    SAS,
+    SELF_SIGNED,
+    CERTIFICATE_AUTHORITY
 }
