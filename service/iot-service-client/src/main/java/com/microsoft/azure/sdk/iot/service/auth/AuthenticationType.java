1539d7dba883a3a910bcae02e0f881b90631c533
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2022-09-20 23:37:51 +0200

    fix the authentcationType enums values

--- service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/auth/AuthenticationType.java
+++ service/iot-service-client/src/main/java/com/microsoft/azure/sdk/iot/service/auth/AuthenticationType.java
@@ -11,7 +11,36 @@ package com.microsoft.azure.sdk.iot.service.auth;
  */
 public enum AuthenticationType
 {
-    SAS,
-    SELF_SIGNED,
-    CERTIFICATE_AUTHORITY
+    SAS(0,"sas"),
+    SELF_SIGNED(1, "selfSigned"),
+    CERTIFICATE_AUTHORITY(2,"certificateAuthority");
+
+    private int key;
+    private String value;
+
+    AuthenticationType(int key, String value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return this.value;
+    }
 }
