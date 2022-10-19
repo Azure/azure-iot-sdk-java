7931ae2bf18d01b96ea7f3ce35344acb65fc96a2
Tim Taylor <timtay@microsoft.com>
2022-10-18 17:32:38 -0700

    Consolidate package version into main pom.xml (#1615)
    The only spots we need to update each release were in the main pom.xml and in these utility classes. With this change, the utility classes will just read this version number from the main pom.xml.
    
    With this change, our releases process will only require bumping the versions of each package in one spot, and we can remove some release pipeline steps

--- provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/internal/SDKUtils.java
+++ provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/internal/SDKUtils.java
@@ -7,11 +7,21 @@
 
 package com.microsoft.azure.sdk.iot.provisioning.device.internal;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+@Slf4j
 public class SDKUtils
 {
     private static final String SERVICE_API_VERSION = "2019-03-31";
     public static final String PROVISIONING_DEVICE_CLIENT_IDENTIFIER = "com.microsoft.azure.sdk.iot.dps.dps-device-client/";
-    public static final String PROVISIONING_DEVICE_CLIENT_VERSION = "2.0.2";
+    public static final String PROVISIONING_DEVICE_CLIENT_VERSION = getPackageVersion();
 
     private static final String JAVA_RUNTIME = System.getProperty("java.version");
     private static final String OPERATING_SYSTEM = System.getProperty("java.runtime.name").toLowerCase().contains("android") ? "Android" : System.getProperty("os.name");
@@ -25,5 +35,33 @@ public class SDKUtils
     public static String getUserAgentString()
     {
         return PROVISIONING_DEVICE_CLIENT_IDENTIFIER + PROVISIONING_DEVICE_CLIENT_VERSION + " (" + JAVA_RUNTIME + "; " + OPERATING_SYSTEM +"; " + PROCESSOR_ARCHITECTURE + ")";
+    }
+
+    // Gets the version of this SDK package from the provisioning-device-client.properties file
+    private static String getPackageVersion()
+    {
+        Map<String, String> properties = getProperties("provisioning-device-client.properties");
+        return properties.getOrDefault("version", "UnknownVersion");
+    }
+
+    private static Map<String, String> getProperties(String propertiesFileName)
+    {
+        try (InputStream inputStream = SDKUtils.class.getClassLoader().getResourceAsStream(propertiesFileName))
+        {
+            if (inputStream != null)
+            {
+                Properties properties = new Properties();
+                properties.load(inputStream);
+                return Collections.unmodifiableMap(properties.entrySet().stream()
+                    .collect(Collectors.toMap(entry -> (String) entry.getKey(),
+                        entry -> (String) entry.getValue())));
+            }
+        }
+        catch (IOException ex)
+        {
+            log.warn("Failed to get properties from " + propertiesFileName, ex);
+        }
+
+        return Collections.emptyMap();
     }
 }
