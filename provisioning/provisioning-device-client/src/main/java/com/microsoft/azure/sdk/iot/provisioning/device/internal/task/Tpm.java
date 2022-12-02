f4c8a0d4416137f75424baf3c6859183091c6042
ngastelum-ms <109701188+ngastelum-ms@users.noreply.github.com>
2022-12-02 13:25:56 -0800

    Update provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/internal/task/Tpm.java
    Co-authored-by: Abhipsa Misra <abhipsa.misra@microsoft.com>

--- provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/internal/task/Tpm.java
+++ provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/internal/task/Tpm.java
@@ -2,7 +2,7 @@ package com.microsoft.azure.sdk.iot.provisioning.device.internal.task;
 
 import com.microsoft.azure.sdk.iot.provisioning.device.TpmRegistrationResult;
 
-public class Tpm extends TpmRegistrationResult
+public class TpmRegistrationResultInternal extends TpmRegistrationResult
 {
     void setAuthenticationKey(String authenticationKey)
     {
