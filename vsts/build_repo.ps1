916f89cba79e53cba36bfeb66d955f399cdf2465
timtay-microsoft <timtay@microsoft.com>
2023-03-07 16:33:17 -0800

    fixup

--- vsts/build_repo.ps1
+++ vsts/build_repo.ps1
@@ -21,17 +21,17 @@ if (($env:JAVA_VERSION).equals("8"))
     if ($isPullRequestBuild.equals("true"))
     {
         Write-Host "Skipping e2e tests since only Java 11 runs e2e tests during a pull request build"
-        mvn install -DskipIntegrationTests -DskipTests -T 2C
+        mvn install -DskipIntegrationTests -T 2C --batch-mode -q
     }
     else
     {
-        mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C
+        mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" verify -T 2C --batch-mode -q
     }
 }
 elseif (($env:JAVA_VERSION).equals("11"))
 {
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
-    mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests=true
+    mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" verify -T 2C -DskipUnitTests --batch-mode -q
 }
 # Leaving this commented out to make it easy to add Java 17 support later
 #elseif (($env:JAVA_VERSION).equals("17"))
