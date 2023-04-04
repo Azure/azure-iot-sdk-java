e2d0099e265e14674466bff0e665faf6ec4a0b7c
Tim Taylor <timtay@microsoft.com>
2023-04-04 10:47:09 -0700

    Remove unused env vars from e2e tests (#1698)
    Collapsing the android/linux/windows env vars as well since they currently use the same value

--- iot-e2e-tests/common/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/helpers/TestConstants.java
+++ iot-e2e-tests/common/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/helpers/TestConstants.java
@@ -8,8 +8,6 @@ package tests.integration.com.microsoft.azure.sdk.iot.helpers;
 public class TestConstants
 {
     public static String IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME = "IOTHUB_CONNECTION_STRING";
-    public static String STORAGE_ACCOUNT_CONNECTION_STRING_ENV_VAR_NAME = "STORAGE_ACCOUNT_CONNECTION_STRING";
-    public static String UNTRUSTWORTHY_IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME = "IOTHUB_CONN_STRING_INVALIDCERT";
     public static String IS_BASIC_TIER_HUB_ENV_VAR_NAME = "IS_BASIC_TIER_HUB";
     public static String IS_PULL_REQUEST = "IS_PULL_REQUEST";
 }
