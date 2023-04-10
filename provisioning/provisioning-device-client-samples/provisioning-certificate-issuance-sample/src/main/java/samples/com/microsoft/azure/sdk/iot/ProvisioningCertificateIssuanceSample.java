abfe97ae7d80ce2cf3e9185d67fcacde3b659479
Nick Patilsen <npatilsen@microsoft.com>
2023-04-10 10:53:34 -0700

    generate sample

---
+++ provisioning/provisioning-device-client-samples/provisioning-certificate-issuance-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/ProvisioningCertificateIssuanceSample.java
@@ -0,0 +1,212 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package samples.com.microsoft.azure.sdk.iot;
+
+import com.microsoft.azure.sdk.iot.device.*;
+import com.microsoft.azure.sdk.iot.device.exceptions.IotHubClientException;
+import com.microsoft.azure.sdk.iot.provisioning.device.*;
+import com.microsoft.azure.sdk.iot.provisioning.device.internal.exceptions.ProvisioningDeviceClientException;
+import com.microsoft.azure.sdk.iot.provisioning.security.SecurityProvider;
+import com.microsoft.azure.sdk.iot.provisioning.security.hsm.SecurityProviderX509Cert;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.security.Key;
+import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Provisioning Sample for Certificate Issuance
+ */
+@SuppressWarnings("CommentedOutCode") // Ignored in samples as we use these comments to show other options.
+public class ProvisioningCertificateIssuanceSample
+{
+    private static final String idScope = "[Your ID scope here]";
+    private static final String globalEndpoint = "[Your Provisioning Service Global Endpoint here]";
+    private static final ProvisioningDeviceClientTransportProtocol PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL = ProvisioningDeviceClientTransportProtocol.HTTPS;
+    //private static final ProvisioningDeviceClientTransportProtocol PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL = ProvisioningDeviceClientTransportProtocol.AMQPS;
+    //private static final ProvisioningDeviceClientTransportProtocol PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL = ProvisioningDeviceClientTransportProtocol.AMQPS_WS;
+    //private static final ProvisioningDeviceClientTransportProtocol PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL = ProvisioningDeviceClientTransportProtocol.MQTT;
+    //private static final ProvisioningDeviceClientTransportProtocol PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL = ProvisioningDeviceClientTransportProtocol.MQTT_WS;
+    private static final int MAX_TIME_TO_WAIT_FOR_REGISTRATION = 10000; // in milli seconds
+    private static final String leafPublicPem = "<Your Public Leaf Certificate Here>";
+    private static final String leafPrivateKeyPem = "<Your Leaf Key Here>";
+
+    private static final Collection<String> signerCertificatePemList = new LinkedList<>();
+
+    static class ProvisioningStatus
+    {
+        ProvisioningDeviceClientRegistrationResult provisioningDeviceClientRegistrationInfoClient = new ProvisioningDeviceClientRegistrationResult();
+        Exception exception;
+    }
+
+    static class ProvisioningDeviceClientRegistrationCallbackImpl implements ProvisioningDeviceClientRegistrationCallback
+    {
+        @Override
+        public void run(ProvisioningDeviceClientRegistrationResult provisioningDeviceClientRegistrationResult, Exception exception, Object context)
+        {
+            if (context instanceof ProvisioningStatus)
+            {
+                ProvisioningStatus status = (ProvisioningStatus) context;
+                status.provisioningDeviceClientRegistrationInfoClient = provisioningDeviceClientRegistrationResult;
+                status.exception = exception;
+            }
+            else
+            {
+                System.out.println("Received unknown context");
+            }
+        }
+    }
+
+    private static class MessageSentCallbackImpl implements MessageSentCallback
+    {
+        @Override
+        public void onMessageSent(Message sentMessage, IotHubClientException exception, Object callbackContext)
+        {
+            System.out.println("Message sent!");
+        }
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Starting...");
+        System.out.println("Beginning setup.");
+        ProvisioningDeviceClient provisioningDeviceClient = null;
+        DeviceClient deviceClient = null;
+        try
+        {
+            ProvisioningStatus provisioningStatus = new ProvisioningStatus();
+
+            // For group enrollment uncomment this line
+            //signerCertificatePemList.add("<Your Signer/intermediate Certificate Here>");
+
+            X509Certificate leafPublicCert = parsePublicKeyCertificate(leafPublicPem);
+            Key leafPrivateKey = parsePrivateKey(leafPrivateKeyPem);
+            Collection<X509Certificate> signerCertificates = new LinkedList<>();
+            for (String signerCertificatePem : signerCertificatePemList)
+            {
+                signerCertificates.add(parsePublicKeyCertificate(signerCertificatePem));
+            }
+
+            SecurityProvider securityProviderX509 = new SecurityProviderX509Cert(leafPublicCert, leafPrivateKey, signerCertificates);
+            provisioningDeviceClient = ProvisioningDeviceClient.create(globalEndpoint, idScope, PROVISIONING_DEVICE_CLIENT_TRANSPORT_PROTOCOL,
+                                                                       securityProviderX509);
+
+            provisioningDeviceClient.registerDevice(new ProvisioningDeviceClientRegistrationCallbackImpl(), provisioningStatus);
+
+            while (provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getProvisioningDeviceClientStatus() != ProvisioningDeviceClientStatus.PROVISIONING_DEVICE_STATUS_ASSIGNED)
+            {
+                if (provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getProvisioningDeviceClientStatus() == ProvisioningDeviceClientStatus.PROVISIONING_DEVICE_STATUS_ERROR ||
+                        provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getProvisioningDeviceClientStatus() == ProvisioningDeviceClientStatus.PROVISIONING_DEVICE_STATUS_DISABLED ||
+                        provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getProvisioningDeviceClientStatus() == ProvisioningDeviceClientStatus.PROVISIONING_DEVICE_STATUS_FAILED )
+
+                {
+                    provisioningStatus.exception.printStackTrace();
+                    System.out.println("Registration error, bailing out");
+                    break;
+                }
+                System.out.println("Waiting for Provisioning Service to register");
+                Thread.sleep(MAX_TIME_TO_WAIT_FOR_REGISTRATION);
+            }
+
+            if (provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getProvisioningDeviceClientStatus() == ProvisioningDeviceClientStatus.PROVISIONING_DEVICE_STATUS_ASSIGNED)
+            {
+                System.out.println("IotHUb Uri : " + provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getIothubUri());
+                System.out.println("Device ID : " + provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getDeviceId());
+
+                // connect to iothub
+                String iotHubUri = provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getIothubUri();
+                String deviceId = provisioningStatus.provisioningDeviceClientRegistrationInfoClient.getDeviceId();
+                try
+                {
+                    deviceClient = new DeviceClient(iotHubUri, deviceId, securityProviderX509, IotHubClientProtocol.MQTT);
+                    deviceClient.open(false);
+                    Message messageToSendFromDeviceToHub =  new Message("Whatever message you would like to send");
+
+                    System.out.println("Sending message from device to IoT Hub...");
+                    deviceClient.sendEventAsync(messageToSendFromDeviceToHub, new MessageSentCallbackImpl(), null);
+                }
+                catch (IOException e)
+                {
+                    System.out.println("Device client threw an exception: " + e.getMessage());
+                    if (deviceClient != null)
+                    {
+                        deviceClient.close();
+                    }
+                }
+            }
+        }
+        catch (ProvisioningDeviceClientException | InterruptedException e)
+        {
+            System.out.println("Provisioning Device Client threw an exception" + e.getMessage());
+            if (provisioningDeviceClient != null)
+            {
+                provisioningDeviceClient.close();
+            }
+        }
+
+        System.out.println("Press any key to exit...");
+
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
+        scanner.nextLine();
+
+        System.out.println("Shutting down...");
+        if (provisioningDeviceClient != null)
+        {
+            provisioningDeviceClient.close();
+        }
+        if (deviceClient != null)
+        {
+            deviceClient.close();
+        }
+    }
+
+    private static Key parsePrivateKey(String privateKeyString) throws IOException
+    {
+        Security.addProvider(new BouncyCastleProvider());
+        PEMParser privateKeyParser = new PEMParser(new StringReader(privateKeyString));
+        Object possiblePrivateKey = privateKeyParser.readObject();
+        return getPrivateKey(possiblePrivateKey);
+    }
+
+    private static X509Certificate parsePublicKeyCertificate(String publicKeyCertificateString) throws IOException, CertificateException
+    {
+        Security.addProvider(new BouncyCastleProvider());
+        PemReader publicKeyCertificateReader = new PemReader(new StringReader(publicKeyCertificateString));
+        PemObject possiblePublicKeyCertificate = publicKeyCertificateReader.readPemObject();
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(possiblePublicKeyCertificate.getContent()));
+    }
+
+    private static Key getPrivateKey(Object possiblePrivateKey) throws IOException
+    {
+        if (possiblePrivateKey instanceof PEMKeyPair)
+        {
+            return new JcaPEMKeyConverter().getKeyPair((PEMKeyPair) possiblePrivateKey)
+                .getPrivate();
+        }
+        else if (possiblePrivateKey instanceof PrivateKeyInfo)
+        {
+            return new JcaPEMKeyConverter().getPrivateKey((PrivateKeyInfo) possiblePrivateKey);
+        }
+        else
+        {
+            throw new IOException("Unable to parse private key, type unknown");
+        }
+    }
+}
