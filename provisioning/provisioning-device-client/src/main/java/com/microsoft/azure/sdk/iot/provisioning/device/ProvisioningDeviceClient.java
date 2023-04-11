0b8344df669f07043f119631939d3bc7f18bb1c7
Nick Patilsen <npatilsen@microsoft.com>
2023-04-11 09:26:11 -0700

    remove srs comments

--- provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/ProvisioningDeviceClient.java
+++ provisioning/provisioning-device-client/src/main/java/com/microsoft/azure/sdk/iot/provisioning/device/ProvisioningDeviceClient.java
@@ -47,29 +47,24 @@ public class ProvisioningDeviceClient
     {
         if (globalEndpoint == null || globalEndpoint.isEmpty())
         {
-            //SRS_ProvisioningDeviceClient_25_001: [ The constructor shall throw IllegalArgumentException if globalEndpoint is null or empty. ]
             throw new IllegalArgumentException("global endpoint cannot be null or empty");
         }
 
         if (idScope == null || idScope.isEmpty())
         {
-            //SRS_ProvisioningDeviceClient_25_002: [ The constructor shall throw IllegalArgumentException if idScope is null or empty. ]
             throw new IllegalArgumentException("scope id cannot be null or empty");
         }
 
         if (protocol == null)
         {
-            //SRS_ProvisioningDeviceClient_25_003: [ The constructor shall throw IllegalArgumentException if protocol is null. ]
             throw new IllegalArgumentException("protocol cannot be null");
         }
 
         if (securityProvider == null)
         {
-            //SRS_ProvisioningDeviceClient_25_004: [ The constructor shall throw IllegalArgumentException if securityProvider is null. ]
             throw new IllegalArgumentException("Security provider cannot be null");
         }
 
-        //SRS_ProvisioningDeviceClient_25_005: [ The constructor shall create provisioningDeviceClientConfig and set all the provided values to it.. ]
         this.provisioningDeviceClientConfig = new ProvisioningDeviceClientConfig();
 
         this.provisioningDeviceClientConfig.setProvisioningServiceGlobalEndpoint(globalEndpoint);
@@ -77,9 +72,7 @@ public class ProvisioningDeviceClient
         this.provisioningDeviceClientConfig.setProtocol(protocol);
         this.provisioningDeviceClientConfig.setSecurityProvider(securityProvider);
 
-        //SRS_ProvisioningDeviceClient_25_006: [ The constructor shall create provisioningDeviceClientContract with the given config. ]
         this.provisioningDeviceClientContract = ProvisioningDeviceClientContract.createProvisioningContract(this.provisioningDeviceClientConfig);
-        //SRS_ProvisioningDeviceClient_25_007: [ The constructor shall create an executor service with fixed thread pool of size 1. ]
         this.executor = Executors.newFixedThreadPool(MAX_THREADS_TO_RUN);
 
         log.debug("Initialized a ProvisioningDeviceClient instance using SDK version {}", SDKUtils.PROVISIONING_DEVICE_CLIENT_VERSION);
@@ -96,14 +89,11 @@ public class ProvisioningDeviceClient
     {
         if (provisioningDeviceClientRegistrationCallback == null)
         {
-            //SRS_ProvisioningDeviceClient_25_008: [ This method shall throw IllegalArgumentException if provisioningDeviceClientRegistrationCallback is null. ]
             throw new IllegalArgumentException("registration callback cannot be null");
         }
 
-        //SRS_ProvisioningDeviceClient_25_009: [ This method shall set the config with the callback. ]
         this.provisioningDeviceClientConfig.setRegistrationCallback(provisioningDeviceClientRegistrationCallback, context);
 
-        //SRS_ProvisioningDeviceClient_25_010: [ This method shall start the executor with the ProvisioningTask. ]
         log.debug("Starting provisioning thread...");
         Callable<Object> provisioningTask = new ProvisioningTask(this.provisioningDeviceClientConfig, this.provisioningDeviceClientContract);
         executor.submit(provisioningTask);
@@ -121,16 +111,13 @@ public class ProvisioningDeviceClient
     {
         if (provisioningDeviceClientRegistrationCallback == null)
         {
-            //SRS_ProvisioningDeviceClient_25_008: [ This method shall throw IllegalArgumentException if provisioningDeviceClientRegistrationCallback is null. ]
             throw new IllegalArgumentException("registration callback cannot be null");
         }
 
         this.provisioningDeviceClientConfig.setPayload(additionalData.getProvisioningPayload());
 
-        //SRS_ProvisioningDeviceClient_25_009: [ This method shall set the config with the callback. ]
         this.provisioningDeviceClientConfig.setRegistrationCallback(provisioningDeviceClientRegistrationCallback, context);
 
-        //SRS_ProvisioningDeviceClient_25_010: [ This method shall start the executor with the ProvisioningTask. ]
         log.debug("Starting provisioning thread...");
         ProvisioningTask provisioningTask = new ProvisioningTask(this.provisioningDeviceClientConfig, this.provisioningDeviceClientContract);
         executor.submit(provisioningTask);
@@ -141,7 +128,6 @@ public class ProvisioningDeviceClient
      */
     public void close()
     {
-        //SRS_ProvisioningDeviceClient_25_011: [ This method shall check if executor is terminated and if not shall shutdown the executor. ]
         if (executor != null && !executor.isTerminated())
         {
             executor.shutdownNow();
