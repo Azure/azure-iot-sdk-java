1171f73131956490be0ba53e4b8f12b0da97d3e5
timtay-microsoft <timtay@microsoft.com>
2023-04-28 11:00:39 -0700

    logs

--- vsts/gradle_build.ps1
+++ vsts/gradle_build.ps1
@@ -20,7 +20,7 @@ else
 }
 
 cd ./iot-e2e-tests/android
-gradle wrapper --gradle-version 7.3.1 --warning-mode all --stacktrace --debug --scan
+gradle wrapper --gradle-version 7.3.1 --warning-mode all --stacktrace --scan
 gradlew :clean :app:clean :app:assembleDebug
 
 gradlew :app:assembleDebugAndroidTest -PIOTHUB_CONNECTION_STRING=$env:IOTHUB_CONNECTION_STRING -PIOT_DPS_CONNECTION_STRING=$env:IOT_DPS_CONNECTION_STRING -PIOT_DPS_ID_SCOPE=$env:DEVICE_PROVISIONING_SERVICE_ID_SCOPE -PDPS_GLOBALDEVICEENDPOINT_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_GLOBAL_ENDPOINT -PPROVISIONING_CONNECTION_STRING_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_CONNECTION_STRING -PIS_BASIC_TIER_HUB=$env:IS_BASIC_TIER_HUB -PIS_PULL_REQUEST=$env:isPullRequestBuild -PRECYCLE_TEST_IDENTITIES=true
