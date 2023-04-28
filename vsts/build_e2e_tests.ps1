d4445fdac96770e1c34009a6f223a5ff5636e551
timtay-microsoft <timtay@microsoft.com>
2023-04-28 10:54:15 -0700

    powershell semantics

--- vsts/build_e2e_tests.ps1
+++ vsts/build_e2e_tests.ps1
@@ -7,4 +7,4 @@ elseif (($env:JAVA_VERSION).equals("11"))
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
 }
 
-mvn -Dmaven.javadoc.skip=true --projects :iot-e2e-common --also-make clean install -DskipTests --batch-mode -q
+mvn `-Dmaven.javadoc.skip=true --projects :iot-e2e-common --also-make clean install -DskipTests --batch-mode -q
