a233d583bfe0c8b25e8d6f4be09101f6bc66f57d
timtay-microsoft <timtay@microsoft.com>
2023-05-01 12:05:16 -0700

    set 11?

--- vsts/echo_versions.ps1
+++ vsts/echo_versions.ps1
@@ -1,10 +1,12 @@
 ﻿if (($env:JAVA_VERSION).equals("8"))
 {
     $env:JAVA_HOME=$env:JAVA_HOME_8_X64
+    [System.Environment]::SetEnvironmentVariable('JAVA_HOME', $env:JAVA_HOME_8_X64, 'User')
 }
 elseif (($env:JAVA_VERSION).equals("11"))
 {
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
+    [System.Environment]::SetEnvironmentVariable('JAVA_HOME', $env:JAVA_HOME_11_X64, 'User')
 }
 
 mvn -v
