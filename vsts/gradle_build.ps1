fb631eb559659b0383b02849d3a2aedff6e2c863
timtay-microsoft <timtay@microsoft.com>
2023-05-01 19:49:09 -0700

    cleanup

--- vsts/gradle_build.ps1
+++ vsts/gradle_build.ps1
@@ -1,9 +1,25 @@
-
+# This script assumes it is being run from the root of the repository
 cd iot-e2e-tests\android
 
+# This script pulls down this version of gradle because the default version installed on ADO
+# can change over time. This allows us more control over when we want to upgrade gradle versions
 wget https://downloads.gradle-dn.com/distributions/gradle-6.8.3-bin.zip
 unzip -d . gradle-6.8.3-bin.zip
-ls
+
+Write-Host "Starting the Gradle Wrapper"
 ./gradle-6.8.3/bin/gradle wrapper
+
+Write-Host "Assembling the source APK"
 ./gradlew :app:assembleDebug
-./gradlew :app:assembleDebugAndroidTest `-PIOTHUB_CONNECTION_STRING=$env:IOTHUB_CONNECTION_STRING `-PIOT_DPS_CONNECTION_STRING=$env:IOT_DPS_CONNECTION_STRING `-PIOT_DPS_ID_SCOPE=$env:DEVICE_PROVISIONING_SERVICE_ID_SCOPE `-PDPS_GLOBALDEVICEENDPOINT_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_GLOBAL_ENDPOINT `-PPROVISIONING_CONNECTION_STRING_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_CONNECTION_STRING `-PDPS_GLOBALDEVICEENDPOINT=$env:DPS_GLOBALDEVICEENDPOINT `-PIS_BASIC_TIER_HUB=$env:IS_BASIC_TIER_HUB `-PIS_PULL_REQUEST=$env:isPullRequestBuild `-PRECYCLE_TEST_IDENTITIES=true
+
+Write-Host "Assembling the test APK"
+./gradlew :app:assembleDebugAndroidTest `
+    `-PIOTHUB_CONNECTION_STRING=$env:IOTHUB_CONNECTION_STRING `
+    `-PIOT_DPS_CONNECTION_STRING=$env:IOT_DPS_CONNECTION_STRING `
+    `-PIOT_DPS_ID_SCOPE=$env:DEVICE_PROVISIONING_SERVICE_ID_SCOPE `
+    `-PDPS_GLOBALDEVICEENDPOINT_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_GLOBAL_ENDPOINT `
+    `-PPROVISIONING_CONNECTION_STRING_INVALIDCERT=$env:INVALID_DEVICE_PROVISIONING_SERVICE_CONNECTION_STRING `
+    `-PDPS_GLOBALDEVICEENDPOINT=$env:DPS_GLOBALDEVICEENDPOINT `
+    `-PIS_BASIC_TIER_HUB=$env:IS_BASIC_TIER_HUB `
+    `-PIS_PULL_REQUEST=$env:isPullRequestBuild `
+    `-PRECYCLE_TEST_IDENTITIES=true
