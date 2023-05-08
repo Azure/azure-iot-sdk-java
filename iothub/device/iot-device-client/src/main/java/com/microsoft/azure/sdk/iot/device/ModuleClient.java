f14dc456c0c8d26383ee07c126a3b199e8b027e3
brycewang-microsoft <brycewang@microsoft.com>
2023-05-08 16:13:11 -0700

    fix some comments

--- iothub/device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/ModuleClient.java
+++ iothub/device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/ModuleClient.java
@@ -66,7 +66,7 @@ public class ModuleClient extends InternalClient
      *                         or
      *
      *                         HostName=xxxx;DeviceId=xxxx;SharedAccessKey=
-     *                         xxxx;moduleId=xxxx;HostNameGateway=xxxx
+     *                         xxxx;moduleId=xxxx;GatewayHostName=xxxx
      * @param protocol The protocol to use when communicating with the module
      * @throws UnsupportedOperationException if using any protocol besides MQTT, if the connection string is missing
      * the "moduleId" field, or if the connection string uses x509
