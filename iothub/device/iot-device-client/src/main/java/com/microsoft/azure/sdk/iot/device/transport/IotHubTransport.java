48ad98b7418dab4b43fc48eabca1f520abfb9eee
timtay-microsoft <timtay@microsoft.com>
2023-05-30 17:22:41 -0700

    why was this necessary?

--- iothub/device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/IotHubTransport.java
+++ iothub/device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/transport/IotHubTransport.java
@@ -1212,7 +1212,7 @@ public class IotHubTransport implements IotHubListener
 
                         // We need to remove the CorrelatingMessageCallback with the current correlation ID from the map after the received C2D
                         // message has been acknowledged. Otherwise, the size of map will grow endlessly which results in OutOfMemory eventually.
-                        correlationCallbacks.remove(correlationId);
+                        new Thread(() -> correlationCallbacks.remove(correlationId)).start();
                     }
                 }
                 catch (Exception ex)
