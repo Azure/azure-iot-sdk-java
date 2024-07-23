4fa47c128684a3e2712eea129e59eec6c3b1bd32
timtay-microsoft <timtay@microsoft.com>
2024-07-23 14:48:34 -0700

    more

--- vsts/build_repo.ps1
+++ vsts/build_repo.ps1
@@ -14,14 +14,17 @@ else
 
 choco install maven
 
-mvn -v
-
 # Set the Java home equal to the Java version under test. Currently, Azure Devops hosted agents only support Java 8, 11 and 17
 # Since they are the current Java LTS versions
 if (($env:JAVA_VERSION).equals("8"))
 {
     choco install openjdk8 -y
     $env:JAVA_HOME=$env:JAVA_HOME_8_X64
+
+    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+    refreshenv
+
+    mvn -v
 
     if ($isPullRequestBuild.equals("true"))
     {
@@ -37,13 +40,14 @@ elseif (($env:JAVA_VERSION).equals("11"))
 {
     choco install openjdk11 -y
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
+
+    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+    refreshenv
+
+    mvn -v
+
     mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests --batch-mode -q
 }
-# Leaving this commented out to make it easy to add Java 17 support later
-#elseif (($env:JAVA_VERSION).equals("17"))
-#{
-#    $env:JAVA_HOME=$env:JAVA_HOME_17_X64
-#}
 else
 {
     Write-Host "Unrecognized or unsupported JDK version: " $env:JAVA_VERSION
---
+++ vsts/build_repo_sdl.ps1
@@ -0,0 +1,19 @@
+﻿choco install maven
+
+if (($env:JAVA_VERSION).equals("8"))
+{
+    choco install openjdk8 -y
+    $env:JAVA_HOME=$env:JAVA_HOME_8_X64
+}
+elseif (($env:JAVA_VERSION).equals("11"))
+{
+    choco install openjdk11 -y
+    $env:JAVA_HOME=$env:JAVA_HOME_11_X64
+}
+
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+refreshenv
+
+mvn -v
+
+mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests --batch-mode -q
