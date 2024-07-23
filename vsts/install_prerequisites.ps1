90565c05d7f8e9828b728718356e63d86e311ea4
timtay-microsoft <timtay@microsoft.com>
2024-07-23 14:29:36 -0700

    openjdk instead

--- vsts/install_prerequisites.ps1
+++ vsts/install_prerequisites.ps1
@@ -1,11 +1,11 @@
 if (($env:JAVA_VERSION).equals("8"))
 {
-    choco install jdk8 -y
+    choco install openjdk8 -y
     $env:JAVA_HOME=$env:JAVA_HOME_8_X64
 }
 elseif (($env:JAVA_VERSION).equals("11"))
 {
-    choco install jdk11 -y
+    choco install openjdk11 -y
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
 }
 
