581bbec2b30c333ade0ff2348314392d850fd360
timtay-microsoft <timtay@microsoft.com>
2024-07-23 12:06:58 -0700

    accept

--- vsts/install_prerequisites.ps1
+++ vsts/install_prerequisites.ps1
@@ -1,15 +1,15 @@
 if (($env:JAVA_VERSION).equals("8"))
 {
-    choco install jdk8
+    choco install jdk8 -y
     $env:JAVA_HOME=$env:JAVA_HOME_8_X64
 }
 elseif (($env:JAVA_VERSION).equals("11"))
 {
-    choco install jdk11
+    choco install jdk11 -y
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
 }
 
-choco install maven
+choco install maven -y
 
 
 
