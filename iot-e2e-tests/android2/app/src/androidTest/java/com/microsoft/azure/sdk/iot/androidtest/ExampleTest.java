02c6adf604c167e975e9a0969c0066f19119e445
Tim Taylor <timtay@microsoft.com>
2026-01-15 11:09:42 -0800

    dummy project

---
+++ iot-e2e-tests/android2/app/src/androidTest/java/com/microsoft/azure/sdk/iot/androidtest/ExampleTest.java
@@ -0,0 +1,26 @@
+package com.microsoft.azure.sdk.iot.androidtest;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleTest {
+    @Test
+    public void shouldPass() {
+
+    }
+
+    @Test
+    public void shouldFail() {
+        Assert.fail();
+    }
+
+}
