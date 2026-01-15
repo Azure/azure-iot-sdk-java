db256fcf08947cc307b0c683d5c2ca238ddbb5d4
Tim Taylor <timtay@microsoft.com>
2026-01-15 15:39:20 -0800

    ?

--- vsts/StartEmulator.sh
+++ vsts/StartEmulator.sh
@@ -26,7 +26,7 @@ echo ''
 echo 'Waiting for emulator to boot up...'
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot -no-window -no-audio -no-boot-anim -accel on > /dev/null 2>&1 &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d "\r") ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 
 $ANDROID_HOME/platform-tools/adb devices
 
