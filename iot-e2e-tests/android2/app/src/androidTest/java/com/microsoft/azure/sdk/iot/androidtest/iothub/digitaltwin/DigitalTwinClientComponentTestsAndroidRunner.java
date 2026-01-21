4a58154a55d05d7e766ba8605a0bd822428ea6e4
Tim Taylor <timtay@microsoft.com>
2026-01-20 16:24:42 -0800

    digital twin tests

---
+++ iot-e2e-tests/android2/app/src/androidTest/java/com/microsoft/azure/sdk/iot/androidtest/iothub/digitaltwin/DigitalTwinClientComponentTestsAndroidRunner.java
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package com.microsoft.azure.sdk.iot.androidtest.iothub.digitaltwin;
+
+import com.microsoft.azure.sdk.iot.androidtest.testgroup.*;
+
+import tests.integration.com.microsoft.azure.sdk.iot.digitaltwin.DigitalTwinClientComponentTests;
+
+@TestGroup10
+public class DigitalTwinClientComponentTestsAndroidRunner extends DigitalTwinClientComponentTests
+{
+    // Intentionally empty class. The important part of this file is that the class inherits tests
+    // from DigitalTwinClientComponentTests and is assigned to a test group.
+}
