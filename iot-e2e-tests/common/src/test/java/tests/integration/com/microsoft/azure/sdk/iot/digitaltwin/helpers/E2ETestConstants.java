396d1ab46cf7fd148c4da51e7b9176f348a0df2f
Tim Taylor <timtay@microsoft.com>
2026-02-06 16:03:09 -0800

    Remove PnP specific tests, exclude some dependencies from our iot-service-client test dependency
    The com.microsoft.rest.client-runtime dependency we get transitively from iot-service-client is no longer compatible with the latest + secure dependencies, so we need to exclude it from our test dependency.
    
    The only place it was being used was in these legacy pnp tests + samples which are no longer relevant

--- iot-e2e-tests/common/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/digitaltwin/helpers/E2ETestConstants.java
+++
@@ -1,7 +0,0 @@
-package tests.integration.com.microsoft.azure.sdk.iot.digitaltwin.helpers;
-
-public class E2ETestConstants {
-    public static final String IOTHUB_CONNECTION_STRING_ENV_VAR_NAME = "IOTHUB_CONNECTION_STRING";
-    public static final String THERMOSTAT_MODEL_ID = "dtmi:com:example:Thermostat;1";
-    public static final String TEMPERATURE_CONTROLLER_MODEL_ID = "dtmi:com:example:TemperatureController;1";
-}
