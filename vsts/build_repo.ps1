98484ee366723d3b74d46ca280e2f3889ca622ed
Tim Taylor <timtay@microsoft.com>
2026-05-20 10:34:22 -0700

    Java 17 and 21

--- vsts/build_repo.ps1
+++ vsts/build_repo.ps1
@@ -16,6 +16,7 @@ else
 # Since they are the current Java LTS versions
 if (($env:JAVA_VERSION).equals("8"))
 {
+    # see here for where this env var comes from: https://github.com/actions/runner-images/blob/main/images/windows/Windows2025-Readme.md#java
     $env:JAVA_HOME=$env:JAVA_HOME_8_X64
 
     if ($isPullRequestBuild.equals("true"))
@@ -33,11 +34,16 @@ elseif (($env:JAVA_VERSION).equals("11"))
     $env:JAVA_HOME=$env:JAVA_HOME_11_X64
     mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests --batch-mode -q
 }
-# Leaving this commented out to make it easy to add Java 17 support later
-#elseif (($env:JAVA_VERSION).equals("17"))
-#{
-#    $env:JAVA_HOME=$env:JAVA_HOME_17_X64
-#}
+elseif (($env:JAVA_VERSION).equals("17"))
+{
+    $env:JAVA_HOME=$env:JAVA_HOME_17_X64
+    mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests `-Dmaven.javadoc.skip=true --batch-mode -q
+}
+elseif (($env:JAVA_VERSION).equals("21"))
+{
+    $env:JAVA_HOME=$env:JAVA_HOME_21_X64
+    mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_DIGITAL_TESTS="$Env:runDigitalTwinTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" -DIS_PULL_REQUEST="$isPullRequestBuild" install -T 2C -DskipUnitTests `-Dmaven.javadoc.skip=true --batch-mode -q
+}
 else
 {
     Write-Host "Unrecognized or unsupported JDK version: " $env:JAVA_VERSION
